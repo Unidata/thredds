3fea4e601d9683ca2704160f1c742b36cfb7d437
Yuan Ho <yuanho@ucar.edu>
2006-09-11 22:49:25 +0000

    added BZip2ReadException for level2 radar

---
+++ cdm/src/main/java/ucar/unidata/io/bzip2/BZip2ReadException.java
@@ -0,0 +1,54 @@
+/*
+ * $Id: BZip2ReadException.java,v 1.1 2006/09/11 21:10:35 dmurray Exp $
+ *
+ * Copyright 1997-2004 Unidata Program Center/University Corporation for
+ * Atmospheric Research, P.O. Box 3000, Boulder, CO 80307,
+ * support@unidata.ucar.edu.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+
+
+
+package ucar.unidata.io.bzip2;
+
+
+/**
+ * Exception for BZip problems
+ *
+ * @author IDV Development Team
+ * @version $Revision: 1.1 $
+ */
+public class BZip2ReadException extends RuntimeException {
+
+    /**
+     * Create a new BZip2ReadException with no message
+     */
+    public BZip2ReadException() {
+        super();
+    }
+
+    /**
+     * Create a new BZip2ReadException with the message
+     *
+     * @param message   detailed message associated with this exception
+     */
+    public BZip2ReadException(String message) {
+        super(message);
+    }
+
+}
+
