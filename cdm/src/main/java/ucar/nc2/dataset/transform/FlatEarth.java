f35fadbcea73ac5f2ede1a8de60c9b64212e9908
Yuan Ho <yuanho@ucar.edu>
2006-10-18 19:06:01 +0000

    added flat earth project, transfer from John's C code

---
+++ cdm/src/main/java/ucar/nc2/dataset/transform/FlatEarth.java
@@ -0,0 +1,53 @@
+// $Id:Orthographic.java 51 2006-07-12 17:13:13Z caron $
+/*
+ * Copyright 1997-2006 Unidata Program Center/University Corporation for
+ * Atmospheric Research, P.O. Box 3000, Boulder, CO 80307,
+ * support@unidata.ucar.edu.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package ucar.nc2.dataset.transform;
+
+import ucar.nc2.dataset.CoordinateTransform;
+import ucar.nc2.dataset.ProjectionCT;
+import ucar.nc2.dataset.TransformType;
+import ucar.nc2.dataset.NetcdfDataset;
+import ucar.nc2.Variable;
+
+/**
+ * Create a Orthographic Projection from the information in the Coordinate Transform Variable.
+ *
+ * @author caron
+ * @version $Revision:51 $ $Date:2006-07-12 17:13:13Z $
+ */
+public class FlatEarth extends AbstractCoordTransBuilder {
+
+  public String getTransformName() {
+    return "flat_earth";
+  }
+
+  public TransformType getTransformType() {
+    return TransformType.Projection;
+  }
+
+  public CoordinateTransform makeCoordinateTransform(NetcdfDataset ds, Variable ctv) {
+    double lon0 = readAttributeDouble( ctv, "longitude_of_projection_origin");
+    double lat0 = readAttributeDouble( ctv, "latitude_of_projection_origin");
+
+    ucar.unidata.geoloc.projection.FlatEarth proj = new ucar.unidata.geoloc.projection.FlatEarth(lat0, lon0);
+    return new ProjectionCT(ctv.getShortName(), "FGDC", proj);
+  }
+}
