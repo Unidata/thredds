45f6397f9447622b078fe3422dd8e84294cb1107
Yuan Ho <yuanho@ucar.edu>
2007-01-04 18:37:14 +0000

    radar collection

---
+++ cdm/src/main/java/ucar/nc2/dt/StationaryRadarDataset.java
@@ -0,0 +1,15 @@
+
+package ucar.nc2.dt;
+
+import java.util.*;
+
+/**
+ * A collection of dataset containing radial objects with RadialDatasetSweep data type.
+ * @author
+ * @version
+ */
+
+public interface StationaryRadarDataset extends RadialDatasetSweep, ucar.nc2.dt.StationaryRadarCollection {
+
+
+}
