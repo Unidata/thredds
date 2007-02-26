8197813d764fd2c364b31aea113abbf31deccce0
Yuan Ho <yuanho@ucar.edu>
2007-02-26 17:41:30 +0000

    javadoc

--- cdm/src/main/java/ucar/nc2/iosp/cinrad/Cinrad2IOServiceProvider.java
+++ cdm/src/main/java/ucar/nc2/iosp/cinrad/Cinrad2IOServiceProvider.java
@@ -1,4 +1,4 @@
-// $Id:CinradIOServiceProvider.java 63 2006-07-12 21:50:51Z edavis $
+// $Id:Cinrad2IOServiceProvider.java 63 2006-07-12 21:50:51Z edavis $
 /*
  * Copyright 1997-2006 Unidata Program Center/University Corporation for
  * Atmospheric Research, P.O. Box 3000, Boulder, CO 80307,
