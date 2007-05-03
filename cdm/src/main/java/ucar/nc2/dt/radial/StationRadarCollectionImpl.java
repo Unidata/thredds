44d9f79fe754e58472cd4dafe53de67783723afc
Yuan Ho <yuanho@ucar.edu>
2007-05-03 16:54:32 +0000

    change name to stationRadarCollection

---
+++ cdm/src/main/java/ucar/nc2/dt/radial/StationRadarCollectionImpl.java
@@ -0,0 +1,160 @@
+package ucar.nc2.dt.radial;
+
+
+import ucar.nc2.dt.StationRadarCollection;
+import ucar.nc2.dt.Station;
+import ucar.nc2.dt.TypedDatasetImpl;
+import ucar.nc2.util.CancelTask;
+import ucar.nc2.units.DateUnit;
+import ucar.unidata.geoloc.LatLonRect;
+import ucar.unidata.geoloc.LatLonPointImpl;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.IOException;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: yuanho
+ * Date: Feb 13, 2007
+ * Time: 1:28:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class StationRadarCollectionImpl extends TypedDatasetImpl implements StationRadarCollection {
+
+    private StationRadarCollection radarCollection;
+    protected HashMap stations;
+    protected HashMap relTimesList;
+    protected HashMap absTimesList;
+    protected DateUnit timeUnit;
+
+    public StationRadarCollectionImpl() {
+      super();
+    }
+
+    public StationRadarCollectionImpl( StationRadarCollection radarDataset) {
+      this.radarCollection = radarDataset;
+    }
+        /* if (location.startsWith("thredds:")) { // LOOK need to distinguish between a DQC and a Catalog !!
+      location = location.substring(8);
+      DqcFactory dqcFactory = new DqcFactory(true);
+      QueryCapability dqc = dqcFactory.readXML(location);
+      if (dqc.hasFatalError()) {
+        if (null != log) log.append(dqc.getErrorMessages());
+        return null;
+      }
+
+      return ucar.nc2.thredds.DqcStationObsDataset.factory( null, dqc);
+    } */
+    
+    private LatLonRect rect;
+    public LatLonRect getBoundingBox() {
+        if (rect == null) {
+          List stations = null;
+          try {
+            stations = radarCollection.getRadarStations();
+          } catch (IOException e) {
+            return null;
+          }
+          if (stations.size() == 0)
+            return null;
+
+          Station s =  (Station) stations.get(0);
+          LatLonPointImpl llpt = new LatLonPointImpl();
+          llpt.set( s.getLatitude(), s.getLongitude());
+          rect = new LatLonRect(llpt, .001, .001);
+
+          for (int i = 1; i < stations.size(); i++) {
+            s =  (Station) stations.get(i);
+            llpt.set( s.getLatitude(), s.getLongitude());
+            rect.extend( llpt);
+          }
+        }
+
+        return rect;
+    }
+
+    public List getStations(LatLonRect boundingBox, CancelTask cancel) throws IOException {
+        LatLonPointImpl latlonPt = new LatLonPointImpl();
+        ArrayList result = new ArrayList();
+        List stationC = radarCollection.getRadarStations();
+        for (int i = 0; i < stationC.size(); i++) {
+          Station s =  (Station) stationC.get(i);
+          latlonPt.set( s.getLatitude(), s.getLongitude());
+          if (boundingBox.contains( latlonPt))
+            result.add( s);
+          if ((cancel != null) && cancel.isCancel()) return null;
+        }
+        return result;
+    }
+
+    public Station getStation(String name) throws IOException {
+        if (stations == null) {
+          List stationC = null;
+          try {
+            stationC = radarCollection.getRadarStations();
+          } catch (IOException e) {
+            return null;
+          }
+
+          stations = new HashMap( 2*stations.size());
+          for (int i = 0; i < stationC.size(); i++) {
+            Station s =  (Station) stationC.get(i);
+            stations.put( s.getName(), s);
+          }
+        }
+
+        return (Station) stations.get(name);
+    }
+
+     public List getStations( )throws IOException {
+         return null;
+     }
+
+
+
+    public List getStations( CancelTask cancel) throws IOException {
+         if ((cancel != null) && cancel.isCancel()) return null;
+         return  getStations( );
+     }
+
+    public List getStations( LatLonRect boundingBox)throws IOException {
+        LatLonPointImpl latlonPt = new LatLonPointImpl();
+        ArrayList result = new ArrayList();
+        List stationC = radarCollection.getRadarStations();
+        for (int i = 0; i < stationC.size(); i++) {
+          Station s =  (Station) stationC.get(i);
+          latlonPt.set( s.getLatitude(), s.getLongitude());
+          if (boundingBox.contains( latlonPt))
+            result.add( s);
+        }
+        return result;
+
+    }
+
+    public int getStationDataCount( Station s) {
+          return 0;
+    }
+
+    public boolean checkStationProduct(Station s, String product) {
+        return true;
+    }
+
+     public boolean checkStationProduct(String product) {
+         return true;
+     }
+
+
+     public List getStationDataProducts( Station s) {
+         return null;
+     }
+
+
+
+
+
+
+
+}
