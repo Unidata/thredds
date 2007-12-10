33a1a2ef4adc7aa87b585c58bc0e0a9f9029a145
Ethan Davis <edavis@ucar.edu>
2007-12-10 15:51:15 +0000

    TDS WCS: improve range.

--- cdm/src/main/java/thredds/wcs/v1_0_0_Plus/WcsRangeField.java
+++ cdm/src/main/java/thredds/wcs/v1_0_0_Plus/WcsRangeField.java
@@ -1,5 +1,9 @@
 package thredds.wcs.v1_0_0_Plus;
 
+import java.util.List;
+import java.util.Collections;
+import java.util.ArrayList;
+
 /**
  * _more_
  *
@@ -15,12 +19,14 @@ public class WcsRangeField
   private String label;
   private String description;
 
-  public WcsRangeField( String name, String label, String description)
+  private List<Axis> axes;
+
+  public WcsRangeField( String name, String label, String description )
   {
     if ( name == null )
-      throw new IllegalArgumentException( "Range name must be non-null.");
+      throw new IllegalArgumentException( "Range name must be non-null." );
     if ( label == null )
-      throw new IllegalArgumentException( "Range label must be non-null.");
+      throw new IllegalArgumentException( "Range label must be non-null." );
     this.name = name;
     this.label = label;
     this.description = description;
@@ -29,4 +35,26 @@ public class WcsRangeField
   public String getName() { return this.name; }
   public String getLabel() { return this.label; }
   public String getDescription() { return this.description; }
+
+  public static class Axis
+  {
+    private String name;
+    private String label;
+    private String description;
+    private List<String> values;
+
+    public Axis( String name, String label, String description, List<String> values)
+    {
+      this.name = name;
+      this.label = label;
+      this.description = description;
+      this.values = new ArrayList<String>();
+      Collections.copy( this.values, values);
+    }
+
+    public String getName() { return this.name; }
+    public String getLabel() { return this.label; }
+    public String getDescription() { return this.description; }
+    public List<String> getValues() { return Collections.unmodifiableList( this.values); }
+  }
 }
