01e426d20c4d268957dd2d6ec402d8f56af195a7
John Caron <caron@unidata.ucar.edu>
2008-01-18 17:59:44 +0000

    Reverse:    thredds.cataloggen.CatalogGen - move to tds    thredds.crawlabledataset.CrawlableDatasetDods move to tds
    modify build.xml

--- tds/src/main/java/thredds/cataloggen/CatalogGenMain.java
+++ tds/src/main/java/thredds/cataloggen/CatalogGenMain.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 import thredds.cataloggen.config.DatasetSource;
+import thredds.cataloggen.config.CatalogRefInfo;
 import thredds.catalog.InvCatalog;
 import thredds.catalog.InvCatalogImpl;
 
