96a1fee1e01994f515982e075240774a371f035c
Yuan Ho <yuanho@ucar.edu>
2008-04-04 20:38:10 +0000

    add threshold attribute to normal resolution nexrad file.

--- cdm/src/main/java/ucar/nc2/iosp/nexrad2/Nexrad2IOServiceProvider.java
+++ cdm/src/main/java/ucar/nc2/iosp/nexrad2/Nexrad2IOServiceProvider.java
@@ -249,8 +249,8 @@ public class Nexrad2IOServiceProvider extends AbstractIOServiceProvider {
     v.addAttribute( new Attribute("_unsigned", "true"));
     if(rd == 1) {
        v.addAttribute( new Attribute("SNR_threshold" ,firstRecord.getDatatypeSNRThreshhold(datatype)));
-       v.addAttribute( new Attribute("range_folding_threshold" ,firstRecord.getDatatypeRangeFoldingThreshhold(datatype)));
     }
+    v.addAttribute( new Attribute("range_folding_threshold" ,firstRecord.getDatatypeRangeFoldingThreshhold(datatype)));
 
     List<Dimension> dim2 = new ArrayList<Dimension>();
     dim2.add( scanDim);
@@ -379,8 +379,9 @@ public class Nexrad2IOServiceProvider extends AbstractIOServiceProvider {
     v.addAttribute( new Attribute("_unsigned", "true"));
     if(datatype == Level2Record.SPECTRUM_WIDTH_HIGH){
        v.addAttribute( new Attribute("SNR_threshold" ,record.getDatatypeSNRThreshhold(datatype)));
-       v.addAttribute( new Attribute("range_folding_threshold" ,record.getDatatypeRangeFoldingThreshhold(datatype)));
     }
+    v.addAttribute( new Attribute("range_folding_threshold" ,record.getDatatypeRangeFoldingThreshhold(datatype)));
+
     Attribute fromAtt = from.findAttribute(_Coordinate.Axes);
     v.addAttribute( new Attribute(_Coordinate.Axes, fromAtt));
 
