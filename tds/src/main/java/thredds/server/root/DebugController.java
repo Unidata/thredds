0c2f9658d108ac42a9cc19f8434d57978bfaa6c4
Ethan Davis <edavis@ucar.edu>
2008-09-25 22:54:30 +0000

    Add DebugController to TDS Spring setup.

---
+++ tds/src/main/java/thredds/server/root/DebugController.java
@@ -0,0 +1,43 @@
+package thredds.server.root;
+
+import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import thredds.servlet.DebugHandler;
+import thredds.server.config.TdsContext;
+
+/**
+ * _more_
+ *
+ * @author edavis
+ * @since 4.0
+ */
+public class DebugController extends AbstractController
+{
+//  private static org.slf4j.Logger log =
+//          org.slf4j.LoggerFactory.getLogger( HtmlController.class );
+
+  private TdsContext tdsContext;
+
+  public void setTdsContext( TdsContext tdsContext )
+  {
+    this.tdsContext = tdsContext;
+  }
+
+  protected ModelAndView handleRequestInternal( HttpServletRequest req, HttpServletResponse res )
+          throws Exception
+  {
+    String path = req.getServletPath();
+    if ( path == null || path.equals( "" ) )
+      path = req.getPathInfo();
+
+    if ( path.equals( "/debug" ) || path.equals( "/debug/" ) )
+    {
+      DebugHandler.doDebug( null, req, res );
+    }
+    return null; // ToDo 
+  }
+}
