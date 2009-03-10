7f1094d6dcd33969f50fefc6c5f77a3bcfdb0d9b
Ethan Davis <edavis@ucar.edu>
2009-03-10 21:16:02 +0000

    TDS 4.0: - More work on CdmValidator configuration.

--- tds/src/main/java/thredds/server/config/HtmlConfig.java
+++ tds/src/main/java/thredds/server/config/HtmlConfig.java
@@ -78,6 +78,9 @@ public class HtmlConfig
   private String hostInstLogoUrl;
   private String hostInstLogoAlt;
 
+  public HtmlConfig()
+  {}
+
   public void init( String webappName,
                     String webappVersion, String webappVersionBrief,
                     String webappVersionBuildDate,
