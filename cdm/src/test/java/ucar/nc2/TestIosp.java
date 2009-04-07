66de591e8406d9c8632c6b52c77e6644848ad691
Ethan Davis <edavis@ucar.edu>
2009-04-07 20:10:24 +0000

    TDS 4.0 - add GRIB IOSP tests to ucar.nc2 test suite.

--- cdm/src/test/java/ucar/nc2/TestIosp.java
+++ cdm/src/test/java/ucar/nc2/TestIosp.java
@@ -49,6 +49,7 @@ public class TestIosp {
 
   public static junit.framework.Test suite ( ) {
     TestSuite suite= new TestSuite();
+    suite.addTest( new TestSuite( ucar.nc2.iosp.grib.TestIndexUpdating.class));
     suite.addTest( new TestSuite( ucar.nc2.iosp.dmsp.TestDmspIosp.class));
     suite.addTest( new TestSuite( ucar.nc2.iosp.gini.TestGini.class));
     suite.addTest( new TestSuite( ucar.nc2.iosp.nexrad2.TestNexrad2.class));
