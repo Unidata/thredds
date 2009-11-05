dd1d8c307df39df366601b6bd6ddc9c623aa2e22
Don Murray <Don.Murray@noaa.gov>
2009-11-05 12:48:15 +0000

    add in the GempakSoundingIOSP

--- cdm/src/main/java/ucar/nc2/NetcdfFile.java
+++ cdm/src/main/java/ucar/nc2/NetcdfFile.java
@@ -226,6 +226,11 @@ public class NetcdfFile implements ucar.nc2.util.cache.FileCacheable {
       if (loadWarnings) log.info("Cant load class: " + e);
     }
     try {
+      registerIOProvider("ucar.nc2.iosp.gempak.GempakSoundingIOSP");
+    } catch (Throwable e) {
+      if (loadWarnings) log.info("Cant load class: " + e);
+    }
+    try {
       registerIOProvider("ucar.nc2.iosp.uamiv.UAMIVServiceProvider");
     } catch (Throwable e) {
       if (loadWarnings) log.info("Cant load class: " + e);
