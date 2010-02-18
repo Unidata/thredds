30789add6be7fd0338cb95045313a62cabd80f35
Rob Kambic <rkambic@unidata.ucar.edu>
2010-02-18 23:48:02 +0000

    testing RadarServer station work

--- tds/src/test/java/ucar/nc2/thredds/server/TestRadarServer.java
+++ tds/src/test/java/ucar/nc2/thredds/server/TestRadarServer.java
@@ -15,36 +15,39 @@ import java.io.IOException;
 public class TestRadarServer {
   //static String server = "http://motherlode.ucar.edu:8081";
   static String server = "http://localhost:8080/";
-      static String pathStart = "/thredds/radarServer/";
-      static String[] requests = {
-        "catalog.xml",
-        "nexrad/level2/catalog.xml",
-        "nexrad/level3/catalog.xml",
-        "nexrad/level2/stations.xml",
-        "nexrad/level3/stations.xml",  
-        "terminal/level2/stations.xml",
-        "terminal/level2/stations.xml",
-        "nexrad/level2/IDD?stn=KFTG",
-        "nexrad/level3/IDD?var=N0R&stn=FTG",
-        "terminal/level3/IDD?var=TR0&stn=MIA",  
-      };
-
+  static String pathStart = "/thredds/radarServer/";
+  static String[] requests = {
+      "catalog.xml",
+      "nexrad/level2/catalog.xml",
+      "nexrad/level3/catalog.xml",
+      "nexrad/level2/stations.xml",
+      "nexrad/level3/stations.xml",
+      "terminal/level2/stations.xml",
+      "terminal/level2/stations.xml",
+      "nexrad/level2/IDD?stn=KFTG",
+      "nexrad/level3/IDD?var=N0R&stn=FTG",
+      "terminal/level3/IDD?var=TR0&stn=MIA",
+  };
 
 
   public static void main(String args[]) {
-      String outputDir = "C:/data/RadarServer/";
-      System.out.println("Copy inventory files to "+outputDir);
-      for (int i = 0; i < requests.length; i++) {
-        //String invPath = invPaths[i];
-        //String name = StringUtil.replace(invPath,'/',"-");
-        File file = new File(outputDir+  i  +".xml");
-        try {
-          IO.readURLtoFileWithExceptions(server+pathStart+requests[ i ], file);
-          System.out.println("Copied "+requests[ i ]+" to "+file.getName());
-        } catch (IOException ioe) {
-          System.out.println("Failed "+requests[ i ]);
-        }
+    String outputDir;
+    File where = new File("C:/data/RadarServer/");
+    if (where.exists()) {
+      outputDir = where.getPath();
+    } else {
+      outputDir = "/local/robb/data/radar/RadarServer/";
+    }
+    System.out.println("Copy inventory files to " + outputDir);
+    for (int i = 0; i < requests.length; i++) {
+      File file = new File(outputDir + i + ".xml");
+      try {
+        IO.readURLtoFileWithExceptions(server + pathStart + requests[i], file);
+        System.out.println("Copied " + requests[i] + " to " + file.getName());
+      } catch (IOException ioe) {
+        System.out.println("Failed " + requests[i]);
       }
     }
+  }
 
 }
