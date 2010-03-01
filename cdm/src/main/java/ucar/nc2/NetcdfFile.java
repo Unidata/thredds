8881bee7e2fa9e998ce8d29b23d7804afcd68302
Yuan Ho <yuanho@ucar.edu>
2010-03-01 23:53:54 +0000

    new iosp, will improve the java doc later

--- cdm/src/main/java/ucar/nc2/NetcdfFile.java
+++ cdm/src/main/java/ucar/nc2/NetcdfFile.java
@@ -172,6 +172,11 @@ public class NetcdfFile implements ucar.nc2.util.cache.FileCacheable {
       if (loadWarnings) log.info("Cant load class: " + e);
     }
     try {
+      registerIOProvider("ucar.nc2.iosp.nowrad.NOWRadiosp");
+    } catch (Throwable e) {
+      if (loadWarnings) log.info("Cant load class: " + e);
+    }
+    try {
       registerIOProvider("ucar.nc2.iosp.dorade.Doradeiosp");
     } catch (Throwable e) {
       if (loadWarnings) log.info("Cant load class: " + e);
