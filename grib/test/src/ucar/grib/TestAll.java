13fa3a8059319f8e8961bc205a6b84036603c732
Rob Kambic <rkambic@unidata.ucar.edu>
2010-11-15 22:06:51 +0000

    TestAll added junit test for duplicate records.

--- grib/test/src/ucar/grib/TestAll.java
+++ grib/test/src/ucar/grib/TestAll.java
@@ -133,6 +133,9 @@ public class TestAll {
     suite.addTest( ucar.grib.grib2.TestGrib2Indexer.suite());
     suite.addTest( ucar.grib.grib2.TestGrib2Data.suite()); // */
 
+    // Test for record duplication in a Grib file
+    suite.addTest(  ucar.grib.TestForDupsInGribIndex.suite() );
+      
       // suite.addTest( new JUnit4TestAdapter( ucar.grib.grib2.Grib2TablesTest.class ) );
 
 
