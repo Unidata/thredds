3e68a3ca97afaf079c84fc6bcc4b5c558149b190
mhermida <mhermida@unidata.ucar.edu>
2012-03-14 08:01:00 -0600

    All tests depending on tdsContext annotated with @TdsContentRootPath.

--- tds/src/test_framework/java/thredds/servlet/DataRootHandlerTest.java
+++ tds/src/test_framework/java/thredds/servlet/DataRootHandlerTest.java
@@ -11,10 +11,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import thredds.mock.web.MockTdsContextLoader;
+import thredds.mock.web.TdsContentRootPath;
 import thredds.server.config.TdsContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"/WEB-INF/applicationContext-tdsConfig.xml"}, loader=MockTdsContextLoader.class)
+@TdsContentRootPath(path = "/share/testcatalogs/content")
 public class DataRootHandlerTest {
 	
 	@Autowired
