410009fdae612493d602e9c9a0bad541a2c152a1
mhermida <mhermida@unidata.ucar.edu>
2012-04-10 13:07:04 -0600

    Fixing some tests

--- tds/src/test/java/thredds/server/config/ServerConfigTestSuit.java
+++ tds/src/test/java/thredds/server/config/ServerConfigTestSuit.java
@@ -6,6 +6,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import thredds.servlet.DataRootHandlerTest;
+import thredds.servlet.HtmlWriterTest;
+
+
 @RunWith(Suite.class)
 @SuiteClasses(value={TdsContextTest.class, DataRootHandlerTest.class, HtmlWriterTest.class})
 public class ServerConfigTestSuit {
