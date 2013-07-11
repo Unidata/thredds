f2a9f1ce74f259864bd9c7eba2b8404ef91ed98f
JohnLCaron <caron@unidata.ucar.edu>
2013-07-11 14:29:27 -0600

    workshop docs (pass 4)

--- ui/src/main/java/ucar/nc2/ui/ToolsUI.java
+++ ui/src/main/java/ucar/nc2/ui/ToolsUI.java
@@ -5730,7 +5730,7 @@ public class ToolsUI extends JPanel {
     String version;
     try {
       InputStream is = ucar.nc2.ui.util.Resource.getFileResource("/README");
-      if (is == null) return "4.3.17";
+      if (is == null) return "4.3.18";
 // DataInputStream dataIS = new DataInputStream( new BufferedInputStream(ios, 20000));
       BufferedReader dataIS = new BufferedReader(new InputStreamReader(is));
       StringBuilder sbuff = new StringBuilder();
