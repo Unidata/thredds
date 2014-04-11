a0a5c997596b1e4d45a7fdc47f83a3406aa0b5d2
JohnLCaron <caron@unidata.ucar.edu>
2014-04-11 10:43:53 -0600

    replace "+" with "plus" in GRIB names.

--- grib/src/main/java/ucar/nc2/grib/GribUtils.java
+++ grib/src/main/java/ucar/nc2/grib/GribUtils.java
@@ -124,7 +124,7 @@ public class GribUtils {
     if (pos > 0) desc = desc.substring(0, pos);
 
     StringBuilder sb = new StringBuilder(desc.trim());
-    StringUtil2.replace(sb, '+', "and");
+    StringUtil2.replace(sb, '+', "plus");
     StringUtil2.replace(sb, "/. ", "-p_");
     StringUtil2.remove(sb, ";,=[]()");
     return sb.toString();
