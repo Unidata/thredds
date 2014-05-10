029a56bcba0ca737fbb97599224f9a23c2b7f1e9
JohnLCaron <caron@unidata.ucar.edu>
2014-05-10 11:23:17 -0600

    fixes to catching <> injection in the query

--- tds/src/main/java/thredds/servlet/filter/RequestQueryFilter.java
+++ tds/src/main/java/thredds/servlet/filter/RequestQueryFilter.java
@@ -32,7 +32,6 @@
  */
 package thredds.servlet.filter;
 
-import ucar.nc2.util.EscapeStrings;
 import thredds.util.StringValidateEncodeUtils;
 
 import javax.servlet.*;
@@ -43,87 +42,78 @@ import java.net.URLDecoder;
 
 /**
  * Reject any request with an invalid query string.
- *
- * <p>
+ * <p/>
+ * <p/>
  * The query string is considered valid if, after decoding, it is
  * a single-line string. For more details, see
  * {@link thredds.util.StringValidateEncodeUtils#validSingleLineString(String)}  validSingleLineString()}.
- *
- * <p>
+ * <p/>
+ * <p/>
  * <strong>Note:</strong>
  * Currently also rejecting strings that contain any less than ("<"),
  * greater than (">"), or backslash ("\") characters. [May loosen this
  * restriction later.]
- *
- * <p>
+ * <p/>
+ * <p/>
  * <strong>Note:</strong>
  * HttpServletRequest.getQueryString()) is not decoded by default so we run it
  * through URLDecoder.decode().
  *
  * @author edavis
- * @since 3.16.47
  * @see thredds.util.StringValidateEncodeUtils#validSingleLineString(String)
  * @see URLDecoder
+ * @since 3.16.47
  */
-public class RequestQueryFilter
-        implements Filter
-{
-  private org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger( getClass() );
+public class RequestQueryFilter implements Filter {
+  private org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(getClass());
   private boolean allowAngleBrackets = false;
 
-  public void init( FilterConfig filterConfig ) throws ServletException
-  {
-    String s = filterConfig.getInitParameter( "allowAngleBrackets" );
-    if ( s != null && s.equalsIgnoreCase( "true" ))
+  public void init(FilterConfig filterConfig) throws ServletException {
+    String s = filterConfig.getInitParameter("allowAngleBrackets");
+    if (s != null && s.equalsIgnoreCase("true"))
       allowAngleBrackets = true;
   }
 
-  public void destroy()
-  {
+  public void destroy() {
   }
 
-  public void doFilter( ServletRequest servletRequest,
-                        ServletResponse servletResponse,
-                        FilterChain filterChain )
-          throws IOException, ServletException
-  {
-    if ( ! ( servletRequest instanceof HttpServletRequest ) )
-    {
-      log.error( "doFilter(): Not an HTTP request! How did this filter get here?" );
-      filterChain.doFilter( servletRequest, servletResponse );
+  public void doFilter(ServletRequest servletRequest,
+                       ServletResponse servletResponse,
+                       FilterChain filterChain)
+          throws IOException, ServletException {
+
+    if (!(servletRequest instanceof HttpServletRequest)) {
+      log.error("RequestQueryFilter: Not an HTTP request! How did this filter get here?");
+      filterChain.doFilter(servletRequest, servletResponse);
       return;
     }
 
     HttpServletRequest request = (HttpServletRequest) servletRequest;
     HttpServletResponse response = (HttpServletResponse) servletResponse;
     String query = request.getQueryString();
-    if ( query != null )
-    {
+    if (query != null) {
       //String decodedQuery = EscapeStrings.unescapeURLQuery(query);
       while (true) {
-        String decodedQuery = URLDecoder.decode( query, StringValidateEncodeUtils.CHARACTER_ENCODING_UTF_8 );
-      boolean badQuery = false;
-      if ( ! allowAngleBrackets
-             && StringValidateEncodeUtils.containsAngleBracketCharacters( decodedQuery ) )
-        badQuery = true;
+        String decodedQuery = URLDecoder.decode(query, StringValidateEncodeUtils.CHARACTER_ENCODING_UTF_8);
+        boolean badQuery = false;
+        if (!allowAngleBrackets && StringValidateEncodeUtils.containsAngleBracketCharacters(decodedQuery))
+          badQuery = true;
 
-      else if ( StringValidateEncodeUtils.containsBackslashCharacters( decodedQuery )
-                || !StringValidateEncodeUtils.validSingleLineString( decodedQuery ))
-        badQuery = true;
+        else if (StringValidateEncodeUtils.containsBackslashCharacters(decodedQuery)
+                || !StringValidateEncodeUtils.validSingleLineString(decodedQuery))
+          badQuery = true;
 
-      if ( badQuery )
-      {
-          log.debug( "doFilter(): Invalid query string [" + query + "]." );
-          String msg = "Invalid query string ";
-          response.sendError( HttpServletResponse.SC_BAD_REQUEST, msg );
-        return;
-      }
+        if (badQuery) {
+          log.debug("RequestQueryFilter: Invalid query string [" + query + "].");
+          response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid query string");
+          return;
+        }
 
         if (query.equals(decodedQuery)) break;
         query = decodedQuery;
-      } // while
+      } // while !query.equals(decodedQuery)
     }
 
-    filterChain.doFilter( servletRequest, servletResponse );
+    filterChain.doFilter(servletRequest, servletResponse);
   }
 }
