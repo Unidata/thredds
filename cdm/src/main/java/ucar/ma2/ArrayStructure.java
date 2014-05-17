7ef0b3802954041b6aafbfa5b52e3ae3e2952745
JohnLCaron <caron@unidata.ucar.edu>
2014-05-17 09:58:50 -0600

    fix merge

--- cdm/src/main/java/ucar/ma2/ArrayStructure.java
+++ cdm/src/main/java/ucar/ma2/ArrayStructure.java
@@ -999,23 +999,6 @@ public abstract class ArrayStructure extends Array {
   /////////////////////////////////////////////////////////////////////////////////////////////////////
 
   @Override
-  public Array sectionNoReduce(List<Range> ranges) throws InvalidRangeException {
-    Section viewSection = new Section(ranges);
-    // if (viewSection.computeSize() == getSize()) return this;
-
-    ArrayStructureW result = new ArrayStructureW(this.members, viewSection.getShape());
-    int count = 0;
-    Section.Iterator iter = viewSection.getIterator(getShape());
-    while (iter.hasNext()) {
-      int recno = iter.next(null);
-      StructureData sd = getStructureData(recno);
-      result.setStructureData(sd, count++);
-    }
-
-    return result;
-  }
-
-  @Override
   public Array createView(Index index) {
     //    Section viewSection = index.getSection(); / LOOK if we could do this, we could make this work
 
