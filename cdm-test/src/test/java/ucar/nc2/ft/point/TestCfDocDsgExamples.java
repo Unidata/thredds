ae13a979befc417619ce91bef7137f7bcfc475bc
cwardgar <cwardgar@gmail.com>
2014-07-23 04:15:37 -0600

    Added the remaining examples from the CF Conventions doc. * Added code to convert the NcML examples to NetCDF-3.

--- cdm-test/src/test/java/ucar/nc2/ft/point/TestCfDocDsgExamples.java
+++ cdm-test/src/test/java/ucar/nc2/ft/point/TestCfDocDsgExamples.java
@@ -3,9 +3,15 @@ package ucar.nc2.ft.point;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import ucar.nc2.FileWriter2;
+import ucar.nc2.NetcdfFile;
+import ucar.nc2.NetcdfFileWriter;
 import ucar.nc2.constants.FeatureType;
+import ucar.nc2.dataset.NetcdfDataset;
 import ucar.unidata.test.util.TestDir;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -68,10 +74,21 @@ public class TestCfDocDsgExamples {
 
         result.add(new Object[] { "H.5.1.1.ncml", FeatureType.STATION_PROFILE, 120 });
         result.add(new Object[] { "H.5.1.2.ncml", FeatureType.STATION_PROFILE, 60  });
+        result.add(new Object[] { "H.5.2.1.ncml", FeatureType.STATION_PROFILE, 20  });
+        result.add(new Object[] { "H.5.3.1.ncml", FeatureType.STATION_PROFILE, 145 });
 
         return result;
     }
 
+    public static List<Object[]> getSectionDatasets() {
+        List<Object[]> result = new ArrayList<>();
+
+        result.add(new Object[] { "H.6.1.1.ncml", FeatureType.SECTION, 120 });
+        result.add(new Object[] { "H.6.2.1.ncml", FeatureType.SECTION, 20  });
+        result.add(new Object[] { "H.6.3.1.ncml", FeatureType.SECTION, 145 });
+
+        return result;
+    }
 
     @Parameterized.Parameters(name = "{0}")  // Name the tests after the location.
     public static List<Object[]> getTestParameters() {
@@ -82,6 +99,7 @@ public class TestCfDocDsgExamples {
         result.addAll(getProfileDatasets());
         result.addAll(getTrajectoryDatasets());
         result.addAll(getStationProfileDatasets());
+        result.addAll(getSectionDatasets());
 
         return result;
     }
@@ -100,5 +118,36 @@ public class TestCfDocDsgExamples {
     @Test
     public void checkPointDataset() throws IOException {
         assert countExpected == TestPointDatasets.checkPointDataset(location, ftype, show);
+    }
+
+    // Convert all NCML files in cfDocDsgExamplesDir to NetCDF-3 files.
+    public static void main(String[] args) throws IOException {
+        File examplesDir = new File(cfDocDsgExamplesDir);
+        File convertedDir = new File(examplesDir, "converted");
+        convertedDir.mkdirs();
+
+        for (File inputFile : examplesDir.listFiles(new NcmlFilenameFilter())) {
+            String inputFilePath = inputFile.getCanonicalPath();
+            String outputFileName = inputFile.getName().substring(0, inputFile.getName().length() - 2);
+            String outputFilePath = new File(convertedDir, outputFileName).getCanonicalPath();
+            System.out.printf("Writing %s to %s.%n", inputFilePath, outputFilePath);
+
+            try (NetcdfFile ncfileIn = NetcdfDataset.openFile(inputFilePath, null)) {
+                NetcdfFileWriter.Version version = NetcdfFileWriter.Version.netcdf3;
+                FileWriter2 writer = new FileWriter2(ncfileIn, outputFilePath, version, null);
+
+                NetcdfFile ncfileOut = writer.write(null);
+                if (ncfileOut != null) {
+                    ncfileOut.close();
+                }
+            }
+        }
+    }
+
+    private static class NcmlFilenameFilter implements FilenameFilter {
+        @Override
+        public boolean accept(File dir, String name) {
+            return name.endsWith("ncml");
+        }
     }
 }
