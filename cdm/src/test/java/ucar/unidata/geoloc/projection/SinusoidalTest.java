a8a58a5f1ad62d01410fdc79c746a9d76a77da3b
cwardgar <cwardgar@unidata.ucar.edu>
2014-08-14 18:34:51 -0600

    Finished tests for Sinusoidal. * These are all of the cases that I can think of. If they all pass, the bounding box calculation should be correct.

--- cdm/src/test/java/ucar/unidata/geoloc/projection/SinusoidalTest.java
+++ cdm/src/test/java/ucar/unidata/geoloc/projection/SinusoidalTest.java
@@ -7,18 +7,39 @@ import ucar.unidata.geoloc.*;
 public class SinusoidalTest {
     // If we want all of the x-coords in the geographic region to be positive, use this.
     // Will be roughly 20015.8.
-    private static final double false_easting  = new Sinusoidal().latLonToProj(0, 180).getX();
+    private static final double false_easting = new Sinusoidal().latLonToProj(0, 180).getX();
 
     // If we want all of the y-coords in the geographic region to be positive, use this.
     // Will be roughly 10007.9.
     private static final double false_northing = new Sinusoidal().latLonToProj(90, 0).getY();
 
-    // Reproduces issue from ETO-719860:
-    // https://www.unidata.ucar.edu/esupport/staff/index.php?_m=tickets&_a=viewticket&ticketid=24221
     @Test
-    public void testEto719860() {
-        // The map area of the dataset. The upper-right corner is "off the earth", which is causing trouble when
-        // converting from projection coords to lat/lon.
+    public void projToLatLonBB_typical() {
+        // All 4 corners of the bounding box are on the earth.
+        //
+        // Values come from visual inspection in ToolsUI->Grid Viewer
+        // Upper left:  -2201 1111  -> 10.0N 20.1W
+        // Upper right: 2982  111   -> 10.0N 27.23E
+        // Lower left:  -2201 -4446 -> 40.0S 25.84W
+        // Lower right: 2982  -4446 -> 40.0S 35.0E
+        ProjectionPoint upperLeft = new ProjectionPointImpl(-2201, 1111);
+        ProjectionPoint lowerRight = new ProjectionPointImpl(2982, -4446);
+
+        Sinusoidal proj = new Sinusoidal();
+        ProjectionRect projBB = new ProjectionRect(upperLeft, lowerRight);
+        LatLonRect latLonBB = proj.projToLatLonBB(projBB);
+
+        Assert.assertEquals(-25.84, latLonBB.getLonMin(), 0.1);
+        Assert.assertEquals(35.0, latLonBB.getLonMax(), 0.1);
+        Assert.assertEquals(-40.0, latLonBB.getLatMin(), 0.1);
+        Assert.assertEquals(10.0, latLonBB.getLatMax(), 0.1);
+    }
+
+    @Test  // Reproduces issue from ETO-719860
+    public void projToLatLonBB_validBottom() {
+        // Bottom 2 corners of bounding box are on the earth. Box intercepts earth's edge at 2 places.
+        //
+        // These values come from the dataset referenced in ETO-719860.
         double minX = 7783.190324950472;
         double minY = 6672.166430716527;
         double maxX = 8895.140844616471;
@@ -28,43 +49,26 @@ public class SinusoidalTest {
         ProjectionRect projBB = new ProjectionRect(minX, minY, maxX, maxY);
         LatLonRect latLonBB = proj.projToLatLonBB(projBB);
 
-        // Based on visual inspection in ToolsUI's Grid Viewer, these seem to be the correct lat/lon bounds.
+        // Values come from visual inspection in ToolsUI->Grid Viewer
         Assert.assertEquals(140.0, latLonBB.getLonMin(), 0.1);
         Assert.assertEquals(180.0, latLonBB.getLonMax(), 0.1);
-        Assert.assertEquals(60.0,  latLonBB.getLatMin(), 0.1);
+        Assert.assertEquals(60.0, latLonBB.getLatMin(), 0.1);
         Assert.assertEquals(67.11, latLonBB.getLatMax(), 0.1);
     }
 
-
     @Test
-    public void projToLatLonBbValidBottom() {
-        // Values come from visual inspection in ToolsUI->Grid Viewer
-        // Lower left:  2889 3336  -> 30.004 N 30.011E
-        // Lower right: 10055 3336 -> 30.004 N 104.42E
-        // Upper left:  2889 9084  -> 81.697 N 179.98E
-        ProjectionPoint lowerLeftProj  = new ProjectionPointImpl(2889, 3336);
-        ProjectionPoint upperRightProj = new ProjectionPointImpl(10055, 10000);
-
-        Sinusoidal proj = new Sinusoidal();
-        ProjectionRect projBB = new ProjectionRect(lowerLeftProj, upperRightProj);
-        LatLonRect latLonBB = proj.projToLatLonBB(projBB);
-
-        Assert.assertEquals(30.011, latLonBB.getLonMin(), 0.1);
-        Assert.assertEquals(180.0,  latLonBB.getLonMax(), 0.1);
-        Assert.assertEquals(30.004, latLonBB.getLatMin(), 0.1);
-        Assert.assertEquals(81.697, latLonBB.getLatMax(), 0.1);
-    }
-
-    @Test
-    public void projToLatLonBbValidTop() {
+    public void projToLatLonBB_validTop() {
+        // Top 2 corners of bounding box are on the earth. Box intercepts earth's edge at 2 places.
+        //
         // Values come from visual inspection in ToolsUI->Grid Viewer
         // Upper left:  -9070 -2780 -> 25.002S 90.004W
         // Upper right: -3603 -2780 -> 25.002S 35.761W
         // Lower right: -3603 -8854 -> 79.627S 179.99W
-        ProjectionPoint upperLeft  = new ProjectionPointImpl(-9070, -2780);
-        ProjectionPoint lowerRight = new ProjectionPointImpl(-3603, -10000);
+        // But let's use false_easting = 20015.8 and false_northing = 10007.9
+        ProjectionPoint upperLeft  = new ProjectionPointImpl(false_easting + -9070, false_northing + -2780);
+        ProjectionPoint lowerRight = new ProjectionPointImpl(false_easting + -3603, false_northing + -10000);
 
-        Sinusoidal proj = new Sinusoidal();
+        Sinusoidal proj = new Sinusoidal(0, false_easting, false_northing, ProjectionImpl.EARTH_RADIUS);
         ProjectionRect projBB = new ProjectionRect(upperLeft, lowerRight);
         LatLonRect latLonBB = proj.projToLatLonBB(projBB);
 
@@ -75,15 +79,18 @@ public class SinusoidalTest {
     }
 
     @Test
-    public void projToLatLonBbValidLeft() {
+    public void projToLatLonBB_validLeft() {
+        // Left 2 corners of bounding box are on the earth. Box intercepts earth's edge at 2 places.
+        //
         // Values come from visual inspection in ToolsUI->Grid Viewer
         // Upper left:  14480 -2228 -> 20.037S 138.62E
         // Lower left:  14480 -4361 -> 39.224S 168.10E
         // Upper right: 18803 -2228 -> 20.037S 179.99E
-        ProjectionPoint upperLeft  = new ProjectionPointImpl(14480, -2228);
-        ProjectionPoint lowerRight = new ProjectionPointImpl(20000, -4361);
+        // But let's use false_easting = 20015.8 and false_northing = 10007.9
+        ProjectionPoint upperLeft  = new ProjectionPointImpl(false_easting + 14480, false_northing + -2228);
+        ProjectionPoint lowerRight = new ProjectionPointImpl(false_easting + 20000, false_northing + -4361);
 
-        Sinusoidal proj = new Sinusoidal();
+        Sinusoidal proj = new Sinusoidal(0, false_easting, false_northing, ProjectionImpl.EARTH_RADIUS);
         ProjectionRect projBB = new ProjectionRect(upperLeft, lowerRight);
         LatLonRect latLonBB = proj.projToLatLonBB(projBB);
 
@@ -94,15 +101,18 @@ public class SinusoidalTest {
     }
 
     @Test
-    public void projToLatLonBbValidRight() {
+    public void projToLatLonBB_validRight() {
+        // Right 2 corners of bounding box are on the earth. Box intercepts earth's edge at 2 places.
+        //
         // Values come from visual inspection in ToolsUI->Grid Viewer
         // Lower right: -9370 4446  -> 39.985N 109.99W
         // Upper right: -9370 6278  -> 56.465N 152.54W
         // Lower left:  -15334 4446 -> 39.985N 179.99W
-        ProjectionPoint lowerRight = new ProjectionPointImpl(-9370,  4446);
-        ProjectionPoint upperLeft  = new ProjectionPointImpl(-17500, 6278);
+        // But let's use false_easting = 20015.8 and false_northing = 10007.9
+        ProjectionPoint lowerRight = new ProjectionPointImpl(false_easting + -9370,  false_northing + 4446);
+        ProjectionPoint upperLeft  = new ProjectionPointImpl(false_easting + -17500, false_northing + 6278);
 
-        Sinusoidal proj = new Sinusoidal();
+        Sinusoidal proj = new Sinusoidal(0, false_easting, false_northing, ProjectionImpl.EARTH_RADIUS);
         ProjectionRect projBB = new ProjectionRect(upperLeft, lowerRight);
         LatLonRect latLonBB = proj.projToLatLonBB(projBB);
 
@@ -112,131 +122,113 @@ public class SinusoidalTest {
         Assert.assertEquals(56.465,  latLonBB.getLatMax(), 0.1);
     }
 
-
-    // Add test for when ll and lr straddle prime meridian. Max lat should be -+90.
-    // Can we test that with Projection.crossSeam()? Why are some of the implementations so funky?
-
-    // Add tests for the 4 cases of 1 good point.
-
-    // Add test for a projBB that's completely off the earth.
-
-
     @Test
-    public void testCalcMinAndMaxYs_1() {
-        // Along the equator, in the default Sinusoidal, the range of valid x's is (-20016, +20016).
-        double projX = 10_000;
+    public void projToLatLonBB_partiallyValidTop() {
+        // The bottom corners are on the earth, the top corners are not. However, the line formed by the top corners
+        // intersects the earth. As a result, the bounding box intersects the earth's edge at 4 places.
+        //
+        // Values come from visual inspection in ToolsUI->Grid Viewer
+        // Lower left : -4166 8342 -> 75.0N 145.0W
+        // Lower right: 4021  8342 -> 75.0N 140.0E
+        // Upper 1st-from-left: -4166 8671 -> 77.985N 180.0W
+        // Upper 2nd-from-left: -1744 9451 -> 85.0N   180.0W
+        // Upper 3rd-from-left: 1743  9451 -> 85.0N   180.0E
+        // Upper 4th-from-left: 4021  8718 -> 78.404N 180.0E
+        ProjectionPoint lowerLeft  = new ProjectionPointImpl(-4166, 8342);
+        ProjectionPoint upperRight = new ProjectionPointImpl(4021,  9451);
 
         Sinusoidal proj = new Sinusoidal();
-        double[] minAndMaxYs = proj.calcMinAndMaxYsAt(projX);
+        ProjectionRect projBB = new ProjectionRect(lowerLeft, upperRight);
+        LatLonRect latLonBB = proj.projToLatLonBB(projBB);
 
-        ProjectionPoint projMinPoint = new ProjectionPointImpl(projX, minAndMaxYs[0]);
-        ProjectionPoint projMaxPoint = new ProjectionPointImpl(projX, minAndMaxYs[1]);
-
-        Assert.assertEquals(projX, projMinPoint.getX(), 1e-6);
-        Assert.assertEquals(projX, projMaxPoint.getX(), 1e-6);
-
-        LatLonPoint latLonMinPoint = proj.projToLatLon(projMinPoint);
-        LatLonPoint latLonMaxPoint = proj.projToLatLon(projMaxPoint);
-
-        Assert.assertEquals(180, latLonMinPoint.getLongitude(), 1e-6);
-        Assert.assertEquals(180, latLonMaxPoint.getLongitude(), 1e-6);
+        Assert.assertEquals(-180.0, latLonBB.getLonMin(), 0.1);
+        Assert.assertEquals(180.0,  latLonBB.getLonMax(), 0.1);
+        Assert.assertEquals(75.0,   latLonBB.getLatMin(), 0.1);
+        Assert.assertEquals(85.0,   latLonBB.getLatMax(), 0.1);
     }
 
     @Test
-    public void testCalcMinAndMaxYs_2() {
-        double projX = false_easting - 4_545;
+    public void projToLatLonBB_onlyIntercepts() {
+        // Same bounding box as projToLatLonBB_partiallyValidTop(), but the left and right sides have been
+        // extended completely off the earth. None of its corners are on the earth, but it intersects it at 4 places.
+        ProjectionPoint lowerLeft  = new ProjectionPointImpl(-13000, 8342);
+        ProjectionPoint upperRight = new ProjectionPointImpl(15000, 9451);
 
-        Sinusoidal proj = new Sinusoidal(0, false_easting, false_northing, ProjectionImpl.EARTH_RADIUS);
-        double[] minAndMaxYs = proj.calcMinAndMaxYsAt(projX);
+        Sinusoidal proj = new Sinusoidal();
+        ProjectionRect projBB = new ProjectionRect(lowerLeft, upperRight);
+        LatLonRect latLonBB = proj.projToLatLonBB(projBB);
 
-        ProjectionPoint projMinPoint = new ProjectionPointImpl(projX, minAndMaxYs[0]);
-        ProjectionPoint projMaxPoint = new ProjectionPointImpl(projX, minAndMaxYs[1]);
-
-        Assert.assertEquals(projX, projMinPoint.getX(), 1e-6);
-        Assert.assertEquals(projX, projMaxPoint.getX(), 1e-6);
-
-        LatLonPoint latLonMinPoint = proj.projToLatLon(projMinPoint);
-        LatLonPoint latLonMaxPoint = proj.projToLatLon(projMaxPoint);
-
-        Assert.assertEquals(-180, latLonMinPoint.getLongitude(), 1e-6);
-        Assert.assertEquals(-180, latLonMaxPoint.getLongitude(), 1e-6);
+        Assert.assertEquals(-180.0, latLonBB.getLonMin(), 0.1);
+        Assert.assertEquals(180.0,  latLonBB.getLonMax(), 0.1);
+        Assert.assertEquals(75.0,   latLonBB.getLatMin(), 0.1);
+        Assert.assertEquals(85.0,   latLonBB.getLatMax(), 0.1);
     }
 
     @Test
-    public void testCalcMinAndMaxYs_3() {
-        double projX = false_easting;
+    public void projToLatLonBB_includesNorthPole() {
+        // Same bouding box as projToLatLonBB_partiallyValidTop(), but the top was extended past 90°N.
+        // It intersects the earth at 2 places and includes the north pole.
+        ProjectionPoint lowerLeft  = new ProjectionPointImpl(-4166, 8342);
+        ProjectionPoint upperRight = new ProjectionPointImpl(4021, 11111);
 
-        Sinusoidal proj = new Sinusoidal(0, false_easting, false_northing, ProjectionImpl.EARTH_RADIUS);
-        double[] minAndMaxYs = proj.calcMinAndMaxYsAt(projX);
+        Sinusoidal proj = new Sinusoidal();
+        ProjectionRect projBB = new ProjectionRect(lowerLeft, upperRight);
+        LatLonRect latLonBB = proj.projToLatLonBB(projBB);
 
-        ProjectionPoint projMinPoint = new ProjectionPointImpl(projX, minAndMaxYs[0]);
-        ProjectionPoint projMaxPoint = new ProjectionPointImpl(projX, minAndMaxYs[1]);
+        Assert.assertEquals(-180.0, latLonBB.getLonMin(), 0.1);
+        Assert.assertEquals(180.0,  latLonBB.getLonMax(), 0.1);
+        Assert.assertEquals(75.0,   latLonBB.getLatMin(), 0.1);
+        Assert.assertEquals(90.0,   latLonBB.getLatMax(), 0.1);
+    }
 
-        LatLonPoint latLonMinPoint = proj.projToLatLon(projMinPoint);
-        LatLonPoint latLonMaxPoint = proj.projToLatLon(projMaxPoint);
+    @Test
+    public void projToLatLonBB_onlyInterceptsAndPole() {
+        // Same bounding box as projToLatLonBB_partiallyValidTop(), but the left, right, and top sides have been
+        // extended completely off the earth. None of its corners are on the earth, but it intersects it at 2 places
+        // and includes the north pole.
+        ProjectionPoint lowerLeft  = new ProjectionPointImpl(-13000, 8342);
+        ProjectionPoint upperRight = new ProjectionPointImpl(15000, 11111);
 
-        Assert.assertEquals(-90, latLonMinPoint.getLatitude(),  1e-6);
-        Assert.assertEquals(0,   latLonMinPoint.getLongitude(), 1e-6);
+        Sinusoidal proj = new Sinusoidal();
+        ProjectionRect projBB = new ProjectionRect(lowerLeft, upperRight);
+        LatLonRect latLonBB = proj.projToLatLonBB(projBB);
 
-        Assert.assertEquals(+90, latLonMaxPoint.getLatitude(),  1e-6);
-        Assert.assertEquals(0,   latLonMaxPoint.getLongitude(), 1e-6);
+        Assert.assertEquals(-180.0, latLonBB.getLonMin(), 0.1);
+        Assert.assertEquals(180.0,  latLonBB.getLonMax(), 0.1);
+        Assert.assertEquals(75.0,   latLonBB.getLatMin(), 0.1);
+        Assert.assertEquals(90.0,   latLonBB.getLatMax(), 0.1);
+    }
+
+    @Test
+    public void projToLatLonBB_poleAndOneCorner() {
+        // The bounding box includes the south pole and only 1 corner is on the earth.
+        // It intersects the the earth's edges at 2 places.
+        //
+        // Values come from visual inspection in ToolsUI->Grid Viewer
+        // Upper left:  -1388 -6673 -> 60.0S 25.0W
+        // Upper right: 10003 -6673 -> 60.0S 180.0E
+        // Lower left:  -1388 -9565 -> 86.0S 180.0W
+        ProjectionPoint upperLeft  = new ProjectionPointImpl(-1388, -6673);
+        ProjectionPoint lowerRight = new ProjectionPointImpl(12000, -12000);
+
+        Sinusoidal proj = new Sinusoidal();
+        ProjectionRect projBB = new ProjectionRect(upperLeft, lowerRight);
+        LatLonRect latLonBB = proj.projToLatLonBB(projBB);
+
+        Assert.assertEquals(-180.0, latLonBB.getLonMin(), 0.1);
+        Assert.assertEquals(180.0,  latLonBB.getLonMax(), 0.1);
+        Assert.assertEquals(60.0,   latLonBB.getLatMin(), 0.1);
+        Assert.assertEquals(90.0,   latLonBB.getLatMax(), 0.1);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testCalcMinAndMaxYs_4() {
-        // Along the equator, in the default Sinusoidal, the range of valid x's is (-20016, +20016).
-        double projX = 30_000;
+    public void projToLatLonBB_completelyOffTheEarth() {
+        // None of the corners are on the earth and none of the sides intersect its edge.
+        ProjectionPoint upperLeft  = new ProjectionPointImpl(10000, -7000);
+        ProjectionPoint lowerRight = new ProjectionPointImpl(3000,  3000);
 
         Sinusoidal proj = new Sinusoidal();
-        proj.calcMinAndMaxYsAt(projX);  // Should throw IllegalArgumentException.
-    }
-
-    @Test
-    public void testCalcMinAndMaxXs_1() {
-        double projY = false_northing + 3_333;
-
-        Sinusoidal proj = new Sinusoidal(0, false_easting, false_northing, ProjectionImpl.EARTH_RADIUS);
-        double[] minAndMaxXs = proj.calcMinAndMaxXsAt(projY);
-
-        ProjectionPoint projMinPoint = new ProjectionPointImpl(minAndMaxXs[0], projY);
-        ProjectionPoint projMaxPoint = new ProjectionPointImpl(minAndMaxXs[1], projY);
-
-        Assert.assertEquals(projY, projMinPoint.getY(), 1e-6);
-        Assert.assertEquals(projY, projMaxPoint.getY(), 1e-6);
-
-        LatLonPoint latLonMinPoint = proj.projToLatLon(projMinPoint);
-        LatLonPoint latLonMaxPoint = proj.projToLatLon(projMaxPoint);
-
-        Assert.assertEquals(-180, latLonMinPoint.getLongitude(), 1e-6);
-        Assert.assertEquals(+180, latLonMaxPoint.getLongitude(), 1e-6);
-    }
-
-    @Test
-    public void testCalcMinAndMaxXs_2() {
-        double projY = false_northing;
-
-        Sinusoidal proj = new Sinusoidal(0, false_easting, false_northing, ProjectionImpl.EARTH_RADIUS);
-        double[] minAndMaxXs = proj.calcMinAndMaxXsAt(projY);
-
-        ProjectionPoint projMinPoint = new ProjectionPointImpl(minAndMaxXs[0], projY);
-        ProjectionPoint projMaxPoint = new ProjectionPointImpl(minAndMaxXs[1], projY);
-
-        LatLonPoint latLonMinPoint = proj.projToLatLon(projMinPoint);
-        LatLonPoint latLonMaxPoint = proj.projToLatLon(projMaxPoint);
-
-        Assert.assertEquals(0,    latLonMinPoint.getLatitude(),  1e-6);
-        Assert.assertEquals(-180, latLonMinPoint.getLongitude(), 1e-6);
-
-        Assert.assertEquals(0,    latLonMaxPoint.getLatitude(),  1e-6);
-        Assert.assertEquals(+180, latLonMaxPoint.getLongitude(), 1e-6);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testCalcMinAndMaxXs_3() {
-        // Along the prime meridian, in the default Sinusoidal, the range of valid y's is (-10008, +10008).
-        double projY = -150_000;
-
-        Sinusoidal proj = new Sinusoidal();
-        proj.calcMinAndMaxXsAt(projY);  // Should throw IllegalArgumentException.
+        ProjectionRect projBB = new ProjectionRect(upperLeft, lowerRight);
+        proj.projToLatLonBB(projBB);  // Throws IllegalArgumentException.
     }
 }
