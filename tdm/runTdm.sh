346636583fbe117c68a354ed70c97e0a64927d30
JohnLCaron <caron@unidata.ucar.edu>
2014-10-22 14:54:30 -0600

    workshop docs

--- tdm/runTdm.sh
+++ tdm/runTdm.sh
@@ -1,1 +1,1 @@
-"C:/Program Files/Java/jdk1.7.0_45/bin/java" -Xmx2g -server -Dtds.content.root.path="C:/dev/github/thredds/tds/src/test/content" -jar target/tdm-4.5.jar 
+java -Xmx2g -server -Dtds.content.root.path="/machine/tds/content" -jar target/tdm-4.5.jar -tds "http://localhost:8080/"
