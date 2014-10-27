0acc999f4a719c17075f52293534ba0b7228bc15
JohnLCaron <caron@unidata.ucar.edu>
2014-10-27 16:16:09 -0600

    Cleanup H4 unit tests.

---
+++ cdm-test/src/test/java/ucar/nc2/iosp/hdf4/TestH4eosRdAll.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 1998-2014 University Corporation for Atmospheric Research/Unidata
+ *
+ *   Portions of this software were developed by the Unidata Program at the
+ *   University Corporation for Atmospheric Research.
+ *
+ *   Access and use of this software shall impose the following obligations
+ *   and understandings on the user. The user is granted the right, without
+ *   any fee or cost, to use, copy, modify, alter, enhance and distribute
+ *   this software, and any derivative works thereof, and its supporting
+ *   documentation for any purpose whatsoever, provided that this entire
+ *   notice appears in all copies of the software, derivative works and
+ *   supporting documentation.  Further, UCAR requests that the user credit
+ *   UCAR/Unidata in any publications that result from the use of this
+ *   software or in any product that includes this software. The names UCAR
+ *   and/or Unidata, however, may not be used in any advertising or publicity
+ *   to endorse or promote any products or commercial entity unless specific
+ *   written permission is obtained from UCAR/Unidata. The user also
+ *   understands that UCAR/Unidata is not obligated to provide the user with
+ *   any support, consulting, training or assistance of any kind with regard
+ *   to the use, operation and performance of this software nor to provide
+ *   the user with any updates, revisions, new versions or "bug fixes."
+ *
+ *   THIS SOFTWARE IS PROVIDED BY UCAR/UNIDATA "AS IS" AND ANY EXPRESS OR
+ *   IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL UCAR/UNIDATA BE LIABLE FOR ANY SPECIAL,
+ *   INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
+ *   FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ *   NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+ *   WITH THE ACCESS, USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package ucar.nc2.iosp.hdf4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import ucar.nc2.Group;
+import ucar.nc2.NetcdfFile;
+import ucar.nc2.Variable;
+import ucar.unidata.test.util.TestDir;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class TestH4eosRdAll {
+
+  @Parameterized.Parameters
+ 	public static Collection<Object[]> getTestParameters() throws IOException {
+    Collection<Object[]> filenames = new ArrayList<>();
+    H4EosFileFilter ff = new H4EosFileFilter();
+    TestDir.actOnAllParameterized(TestH4eos.testDir, ff, filenames);
+ 		return filenames;
+ 	}
+
+  static class H4EosFileFilter implements java.io.FileFilter {
+    public boolean accept(File pathname) {
+      return pathname.getName().endsWith(".hdf") || pathname.getName().endsWith(".eos");
+    }
+  }
+
+  String filename;
+  public TestH4eosRdAll(String filename) {
+    this.filename = filename;
+  }
+
+  @Test
+  public void testForStructMetadata() throws IOException {
+    System.out.printf("TestH4eosReadAll %s%n", filename);
+    try (NetcdfFile ncfile = NetcdfFile.open(filename)) {
+       Group root = ncfile.getRootGroup();
+       Group g = root.findGroup("HDFEOS INFORMATION");
+       if (g == null) g = ncfile.getRootGroup();
+
+       Variable dset = g.findVariable("StructMetadata.0");
+       assert (dset != null);
+     }
+   }
+
+}
