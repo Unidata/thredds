4ac6fb1e89047d70aedb9bcdfd08f36259aec977
JohnLCaron <caron@unidata.ucar.edu>
2014-10-30 19:41:43 -0600

    AreaServiceProvider leaking file handles

--- visad/src/main/java/ucar/nc2/iosp/mcidas/AreaServiceProvider.java
+++ visad/src/main/java/ucar/nc2/iosp/mcidas/AreaServiceProvider.java
@@ -43,7 +43,6 @@ import ucar.nc2.Variable;
 
 import ucar.nc2.iosp.AbstractIOServiceProvider;
 
-import ucar.nc2.iosp.IOServiceProvider;
 import ucar.nc2.util.CancelTask;
 
 import ucar.unidata.io.RandomAccessFile;
@@ -54,85 +53,74 @@ import java.io.IOException;
 /**
  * IOServiceProvider for McIDAS AREA files
  *
- *
  * @author dmurray
  */
 public class AreaServiceProvider extends AbstractIOServiceProvider {
+  /**
+   * AREA file reader
+   */
+  protected AreaReader areaReader;
 
+  /**
+   * Is this a valid file?
+   *
+   * @param raf RandomAccessFile to check
+   * @return true if a valid McIDAS AREA file
+   * @throws IOException problem reading file
+   */
+  public boolean isValidFile(RandomAccessFile raf) throws IOException {
+    return AreaReader.isValidFile(raf);
+  }
 
-    /** AREA file reader */
-    protected AreaReader areaReader;
+  public String getFileTypeId() {
+    return "McIDASArea";
+  }
 
-    /**
-     * Is this a valid file?
-     *
-     * @param raf  RandomAccessFile to check
-     *
-     * @return true if a valid McIDAS AREA file
-     *
-     * @throws IOException  problem reading file
-     */
-    public boolean isValidFile(RandomAccessFile raf) throws IOException {
-        return AreaReader.isValidFile(raf);
+  public String getFileTypeDescription() {
+    return "McIDAS area file";
+  }
+
+  /**
+   * Open the service provider for reading.
+   *
+   * @param raf        file to read from
+   * @param ncfile     netCDF file we are writing to (memory)
+   * @param cancelTask task for cancelling
+   * @throws IOException problem reading file
+   */
+  public void open(RandomAccessFile raf, NetcdfFile ncfile, CancelTask cancelTask) throws IOException {
+    if (areaReader == null)
+      areaReader = new AreaReader();
+
+    try {
+      areaReader.init(raf, ncfile);
+
+    } catch (Exception e) {
+      close();              // try not to leak files
+      throw new IOException(e);
     }
 
-    public String getFileTypeId() {
-      return "McIDASArea";
-    }
+  }
 
-    public String getFileTypeDescription() {
-      return "McIDAS area file";
-    }
+  /**
+   * Read the data for the variable
+   *
+   * @param v2      Variable to read
+   * @param section section infomation
+   * @return Array of data
+   * @throws IOException           problem reading from file
+   * @throws InvalidRangeException invalid Range
+   */
+  public Array readData(Variable v2, Section section) throws IOException, InvalidRangeException {
+    return areaReader.readVariable(v2, section);
+  }
 
-    /**
-     * Open the service provider for reading.
-     * @param raf  file to read from
-     * @param ncfile  netCDF file we are writing to (memory)
-     * @param cancelTask  task for cancelling
-     *
-     * @throws IOException  problem reading file
-     */
-    public void open(RandomAccessFile raf, NetcdfFile ncfile,
-                     CancelTask cancelTask)
-            throws IOException {
-        long start = System.currentTimeMillis();
-        if (areaReader == null) {
-            areaReader = new AreaReader();
-        }
-        try {
-            areaReader.init(raf, ncfile);
-        } catch (Exception e) {}
-        long end = System.currentTimeMillis() - start;
+  public void close() throws IOException {
+    if (areaReader != null) {
+      if (areaReader.af != null)
+        areaReader.af.close();
+      areaReader = null;
     }
-
-    /**
-     * Read the data for the variable
-     * @param v2  Variable to read
-     * @param section   section infomation
-     * @return Array of data
-     *
-     * @throws IOException problem reading from file
-     * @throws InvalidRangeException  invalid Range
-     */
-    public Array readData(Variable v2, Section section)
-            throws IOException, InvalidRangeException {
-        long  start = System.currentTimeMillis();
-        Array array = areaReader.readVariable(v2, section);
-        long  end   = System.currentTimeMillis() - start;
-        return array;
-    }
-
-    // TODO: what do we do here?
-
-    /**
-     * Close this IOSP
-     *
-     * @throws IOException problem closing file
-     */
-    public void close() throws IOException {
-        if (areaReader != null) {
-            areaReader = null;
-        }
-    }
+  }
 
 }
