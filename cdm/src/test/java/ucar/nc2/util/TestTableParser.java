8be261553252e5615f0c741004d8044cb4527f35
Ryan May <rmay@ucar.edu>
2014-12-17 10:37:52 -0700

    Convert testReadNexradTable into actual test.
    Make the test actually check the results of parsing rather than
    printing. This also reduces the amount of output in test logs.

--- cdm/src/test/java/ucar/nc2/util/TestTableParser.java
+++ cdm/src/test/java/ucar/nc2/util/TestTableParser.java
@@ -1,5 +1,6 @@
 package ucar.nc2.util;
 
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -24,17 +25,26 @@ public class TestTableParser {
     Class c = TableParser.class;
     InputStream is = c.getResourceAsStream(testName3);
     List<TableParser.Record> recs = TableParser.readTable(is, "3,15,54,60d,67d,73d", 50000);
-    for (TableParser.Record record : recs) {
-      for (int j = 0; j < record.values.size(); j++) {
-        Object s = record.values.get(j);
-        System.out.print(" " + s.toString());
-      }
-      System.out.println();
-    }
+
+    TableParser.Record rec = recs.get(0);
+    Assert.assertEquals("TLX", rec.get(0));
+    Assert.assertEquals("      000001", rec.get(1));
+    Assert.assertEquals(" OKLAHOMA_CITY/Norman             OK US", rec.get(2));
+    Assert.assertEquals(3532.0, (Double)rec.get(3), 0.1);
+    Assert.assertEquals(-9727.0, (Double)rec.get(4), 0.1);
+    Assert.assertEquals(370.0, (Double)rec.get(5), 0.1);
+
+    rec = recs.get(20);
+    Assert.assertEquals("TWX", rec.get(0));
+    Assert.assertEquals("      000554", rec.get(1));
+    Assert.assertEquals(" TOPEKA/Alma                      KS US", rec.get(2));
+    Assert.assertEquals(3898.0, (Double)rec.get(3), 0.1);
+    Assert.assertEquals(-9622.0, (Double)rec.get(4), 0.1);
+    Assert.assertEquals(417.0, (Double)rec.get(5), 0.1);
   }
 
   @Test
-  @Ignore("ghcnm.v3.0.0-beta1.20101207.qae.dat isn't in /share/testdata")
+  @Ignore("ghcnm.v3.0.0-beta1.20101207.qae.dat isn't in repo")
   public void testReadgGghcnmTable() throws IOException {
     List<TableParser.Record> recs = TableParser.readTable(testRepeat, "11L,15i,19,(24i,25,26,27)*10", 5);
     for (TableParser.Record record : recs) {
