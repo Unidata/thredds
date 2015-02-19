65208c2630733ee08ac8db2cbfa43d6a85ccb8d8
Ryan May <rmay@ucar.edu>
2015-02-18 17:10:29 -0700

    Make MRMS table parsing fix some units, other typos.
    Regenerate the Java table code.

--- grib/src/main/resources/resources/grib2/mrms/convert_mrms_table.py
+++ grib/src/main/resources/resources/grib2/mrms/convert_mrms_table.py
@@ -14,8 +14,25 @@ def parse_file(fname):
             ret.append(RowEntry(*row))
         return ret
 
+def fix_item(item):
+    # Fix units that have AGL/MSL in them. Add to description instead
+    if item.Unit.endswith(' MSL') or item.Unit.endswith(' AGL'):
+        item = item._replace(Unit=item.Unit[:-4],
+                Description=item.Description + item.Unit[-4:])
+    elif item.Unit == 'non-dim' or item.Unit == 'flag': # Change to cf-compliant string
+        item = item._replace(Unit='dimensionless')
+
+    # Fix spelling errors for reflectivity
+    if 'Reflectivty' in item.Description:
+        item = item._replace(Description=item.Description.replace('Reflectivty', 'Reflectivity'))
+    if 'Reflectivty' in item.Name:
+        item = item._replace(Name=item.Name.replace('Reflectivty', 'Reflectivity'))
+
+    return item
+
 def make_java(info):
     for i in info:
+        i = fix_item(i)
         print('add({0.Discipline}, {0.Category}, {0.Parameter}, "{0.Name}", '
                 '"{0.Description}", "{0.Unit}", {0.No_Coverage}, '
                 '{0.Missing});'.format(i))
