1a326709101f26ebac50e7714a7f7cc4f7824fed
cwardgar <cwardgar@gmail.com>
2015-03-06 09:31:01 -0700

    Got JUnit Category exclusions working. * Right now I'm just experimenting with the grib tests. That'll all be reverted, because none of those tests actually NeedCdmUnitTest. * Had to comment-out rootTestReport. The way I have it configured, any time I run a subproject's tests, it causes ALL tests to run.

---
+++ cdm/src/test/java/ucar/unidata/test/util/NeedsCdmUnitTest.java
@@ -0,0 +1,11 @@
+package ucar.unidata.test.util;
+
+/**
+ * A marker to be used with JUnit categories to indicate that a test method or test class requires access to the
+ * cdmUnitTest shared directory in order to complete successfully.
+ *
+ * @author cwardgar
+ * @since 2015/03/06
+ */
+public interface NeedsCdmUnitTest {
+}
