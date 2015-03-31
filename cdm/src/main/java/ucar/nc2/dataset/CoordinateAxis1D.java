d79befe614f24e4f7fd8a64b920e7ee49967c4cc
JohnLCaron <caron@unidata.ucar.edu>
2015-03-31 16:06:16 -0600

    fix unit tests

--- cdm/src/main/java/ucar/nc2/dataset/CoordinateAxis1D.java
+++ cdm/src/main/java/ucar/nc2/dataset/CoordinateAxis1D.java
@@ -235,7 +235,7 @@ public class CoordinateAxis1D extends CoordinateAxis {
      int n = getDimension(0).getLength();
      List<NamedObject> names = new ArrayList<>(n);
      for (int i = 0; i < n; i++)
-       names.add(new ucar.nc2.util.NamedAnything(getCoordName(i), getUnitsString()));
+       names.add(new ucar.nc2.util.NamedAnything(getCoordName(i), getShortName() + " "+ getUnitsString()));
      return names;
    }
 
