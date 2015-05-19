4c6eb8b21bed058d54579ed88c174bbf3aa93420
Ryan May <rmay@ucar.edu>
2015-05-19 17:14:18 -0600

    Read timeCoverage and geospatialCoverage from config.
    Rather than hard coding these coverages, read them from the
    configuration file, since they're already there. In the future we should
    really calculate these.

--- tds/src/main/java/thredds/server/radarServer2/RadarServerConfig.java
+++ tds/src/main/java/thredds/server/radarServer2/RadarServerConfig.java
@@ -5,6 +5,10 @@ import org.jdom2.Element;
 import org.jdom2.Namespace;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
+import ucar.nc2.constants.CDM;
+import ucar.nc2.units.DateRange;
+import ucar.nc2.units.DateType;
+import ucar.nc2.units.TimeDuration;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by rmay on 3/26/15.
+ * Handle configuration for the Radar Server
  */
 public class RadarServerConfig {
     static public List<RadarConfigEntry> readXML(String filename) {
@@ -41,8 +45,14 @@ public class RadarServerConfig {
                 conf.dataFormat = meta.getChild("dataFormat", catNS).getValue();
                 conf.stationFile = meta.getChild("stationFile", catNS).getAttributeValue("path");
                 conf.doc = meta.getChild("documentation", catNS).getValue();
-                Element variables = meta.getChild("variables", catNS);
 
+                conf.timeCoverage = readTimeCoverage(
+                        meta.getChild("timeCoverage", catNS));
+
+                conf.spatialCoverage = readGeospatialCoverage(
+                        meta.getChild("geospatialCoverage", catNS));
+
+                Element variables = meta.getChild("variables", catNS);
                 conf.vars = new ArrayList<>();
                 for (Element var: variables.getChildren("variable", catNS)) {
                     RadarConfigEntry.VarInfo inf = new RadarConfigEntry.VarInfo();
@@ -60,10 +70,102 @@ public class RadarServerConfig {
         return configs;
     }
 
+    protected static RadarConfigEntry.GeoInfo readGeospatialCoverage(Element gcElem) {
+        if (gcElem == null) return null;
+        Namespace defNS = gcElem.getNamespace();
+
+        RadarConfigEntry.GeoInfo gi = new RadarConfigEntry.GeoInfo();
+        gi.northSouth = readGeospatialRange(gcElem.getChild("northsouth", defNS), CDM.LAT_UNITS);
+        gi.eastWest = readGeospatialRange(gcElem.getChild("eastwest", defNS), CDM.LON_UNITS);
+        gi.upDown = readGeospatialRange(gcElem.getChild("updown", defNS), "m");
+
+        return gi;
+    }
+
+    protected static RadarConfigEntry.RangeInfo readGeospatialRange(Element spElem, String defUnits) {
+        if (spElem == null) return null;
+
+        Namespace defNS = spElem.getNamespace();
+        double start = readDouble(spElem.getChild("start", defNS));
+        double size = readDouble(spElem.getChild("size", defNS));
+
+        String units = spElem.getChildText("units", defNS);
+        if (units == null) units = defUnits;
+
+        RadarConfigEntry.RangeInfo ri = new RadarConfigEntry.RangeInfo();
+        ri.units = units;
+        ri.start = start;
+        ri.size = size;
+        return ri;
+    }
+
+    protected static double readDouble(Element elem) {
+        if (elem == null) return Double.NaN;
+        String text = elem.getText();
+        try {
+            return Double.parseDouble(text);
+        } catch (NumberFormatException e) {
+            return Double.NaN;
+        }
+    }
+
+    protected static DateRange readTimeCoverage(Element tElem) {
+        if (tElem == null) return null;
+        Namespace defNS = tElem.getNamespace();
+        DateType start = readDate(tElem.getChild("start", defNS));
+        DateType end = readDate(tElem.getChild("end", defNS));
+        TimeDuration duration = readDuration(tElem.getChild("duration", defNS));
+        TimeDuration resolution = readDuration(tElem.getChild("resolution", defNS));
+
+        try {
+            return new DateRange(start, end, duration, resolution);
+        } catch (java.lang.IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    protected static DateType readDate(Element elem) {
+        if (elem == null) return null;
+        String format = elem.getAttributeValue("format");
+        String type = elem.getAttributeValue("type");
+        return makeDateType(elem.getText(), format, type);
+    }
+
+    protected static DateType makeDateType(String text, String format, String type) {
+        if (text == null) return null;
+        try {
+            return new DateType(text, format, type);
+        } catch (java.text.ParseException e) {
+            return null;
+        }
+    }
+
+    protected static TimeDuration readDuration(Element elem) {
+        if (elem == null) return null;
+        String text;
+        try {
+            text = elem.getText();
+            return new TimeDuration(text);
+        } catch (java.text.ParseException e) {
+            return null;
+        }
+    }
+
     static public class RadarConfigEntry {
         public String name, urlPath, diskPath, dataFormat, stationFile, doc;
         public String dateParseRegex, dateFmt, layout;
+        public DateRange timeCoverage;
+        public GeoInfo  spatialCoverage;
         public List<VarInfo> vars;
+
+        static public class GeoInfo {
+            RangeInfo eastWest, northSouth, upDown;
+        }
+
+        static public class RangeInfo {
+            public String units;
+            public double start, size;
+        }
 
         static public class VarInfo {
             public String name, vocabName, units;
--- tds/src/main/java/thredds/server/radarServer2/RadarServerController.java
+++ tds/src/main/java/thredds/server/radarServer2/RadarServerController.java
@@ -55,6 +55,8 @@ public class RadarServerController {
     static final String entryPoint = "radarServer/";
     static final String URLbase = appName + entryPoint;
     static Map<String, List<RadarServerConfig.RadarConfigEntry.VarInfo>> vars;
+    static Map<String, DateRange> timeCoverages;
+    static Map<String, RadarServerConfig.RadarConfigEntry.GeoInfo> geoCoverages;
     boolean enabled = false;
 
     @Autowired
@@ -122,6 +124,8 @@ public class RadarServerController {
 
         data = new TreeMap<>();
         vars = new TreeMap<>();
+        timeCoverages = new TreeMap<>();
+        geoCoverages = new TreeMap<>();
         String contentPath = tdsContext.getContentDirectory().getPath();
         List<RadarServerConfig.RadarConfigEntry> configs = RadarServerConfig.readXML(contentPath + "/radar/radarCollections.xml");
         for (RadarServerConfig.RadarConfigEntry conf : configs) {
@@ -150,6 +154,8 @@ public class RadarServerController {
 
             data.put(conf.urlPath, di);
             vars.put(conf.urlPath, conf.vars);
+            timeCoverages.put(conf.urlPath, conf.timeCoverage);
+            geoCoverages.put(conf.urlPath, conf.spatialCoverage);
             StationList sl = di.getStationList();
             sl.loadFromXmlFile(contentPath + "/" + conf.stationFile);
         }
@@ -259,21 +265,19 @@ public class RadarServerController {
                 "summary", di.getDescription()));
 
         // TODO: Pull from inventory
+        RadarServerConfig.RadarConfigEntry.GeoInfo gi = geoCoverages.get(dataset);
         metadata.put(Dataset.GeospatialCoverage,
                 new ThreddsMetadata.GeospatialCoverage(
-                        new ThreddsMetadata.GeospatialRange(20.0, 40.0, 0.0,
-                                "degrees_north"),
-                        new ThreddsMetadata.GeospatialRange(-135.0, 70.0, 0.0,
-                                "degrees_east"),
-                        new ThreddsMetadata.GeospatialRange(0.0, 70.0, 0.0,
-                                "km"), new ArrayList<ThreddsMetadata.Vocab>(),
-                        null));
+                        new ThreddsMetadata.GeospatialRange(gi.eastWest.start,
+                                gi.eastWest.size, 0.0, gi.eastWest.units),
+                        new ThreddsMetadata.GeospatialRange(gi.northSouth.start,
+                                gi.northSouth.size, 0.0, gi.northSouth.units),
+                        new ThreddsMetadata.GeospatialRange(gi.upDown.start,
+                                gi.upDown.size, 0.0, gi.upDown.units),
+                        new ArrayList<ThreddsMetadata.Vocab>(), null));
 
-        CalendarDate now = CalendarDate.present();
-        CalendarDate start = now.subtract(CalendarPeriod.of(14,
-                CalendarPeriod.Field.Day));
-        metadata.put(Dataset.TimeCoverage, new DateRange(start.toDate(),
-                now.toDate()));
+        // TODO: Should come from inventory
+        metadata.put(Dataset.TimeCoverage, timeCoverages.get(dataset));
 
         // TODO: Need to be able to get this from the inventory
         List<ThreddsMetadata.Variable> catalogVars = new ArrayList<>();
