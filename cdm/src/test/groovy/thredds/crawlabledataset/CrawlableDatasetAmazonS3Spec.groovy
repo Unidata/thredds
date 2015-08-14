f60b1ffa4921d32978a666a00ce6b66af7c4c10d
cwardgar <cwardgar@unidata.ucar.edu>
2015-08-14 16:39:28 -0600

    Added CrawlableDatasetAmazonS3Spec and all Spock deps. * May need cglib for mocking of classes (in addition to interfaces).

---
+++ cdm/src/test/groovy/thredds/crawlabledataset/CrawlableDatasetAmazonS3Spec.groovy
@@ -0,0 +1,11 @@
+package thredds.crawlabledataset
+
+import spock.lang.Specification
+
+/**
+ * @author cwardgar
+ * @since 2015/08/14
+ */
+class CrawlableDatasetAmazonS3Spec extends Specification {
+
+}
