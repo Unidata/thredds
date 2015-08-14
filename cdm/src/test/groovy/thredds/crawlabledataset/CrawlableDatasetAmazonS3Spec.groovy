cf0397b7c3f4c81e66df6489e3450be69df826c2
cwardgar <cwardgar@unidata.ucar.edu>
2015-08-14 17:41:35 -0600

    Convert TestCrawlableDatasetAmazonS3 to CrawlableDatasetAmazonS3Spec * Mocking the AWS interactions will be easier with Spock. * Using "aws-java-sdk-s3" instead of "aws-java-sdk". It drags in MUCH fewer dependencies.

--- cdm/src/test/groovy/thredds/crawlabledataset/CrawlableDatasetAmazonS3Spec.groovy
+++ cdm/src/test/groovy/thredds/crawlabledataset/CrawlableDatasetAmazonS3Spec.groovy
@@ -1,11 +1,44 @@
 package thredds.crawlabledataset
 
+import net.sf.ehcache.Element
 import spock.lang.Specification
+
+import static thredds.crawlabledataset.CrawlableDatasetAmazonS3.S3Helper.*
 
 /**
  * @author cwardgar
  * @since 2015/08/14
  */
 class CrawlableDatasetAmazonS3Spec extends Specification {
+    def "S3Helper parent"() {
+        expect:
+        parent("s3://bucket/path/to/object") == "s3://bucket/path/to"
+    }
 
+    def "S3Helper concat"() {
+        expect:
+        concat("s3://bucket/path/to", "object") == "s3://bucket/path/to/object"
+        concat("s3://bucket/path/to", "object/") == "s3://bucket/path/to/object"
+        concat("s3://bucket/path/to", "") == "s3://bucket/path/to"
+    }
+
+    def "S3Helper s3UriParts"() {
+        expect:
+        s3UriParts("s3://bucket/path/to/object") as List == ["bucket", "path/to/object"]
+    }
+
+    def "S3Helper file creation and deletion"() {
+        when:
+        File tmpFile = createTempFile("s3://bucket/path/to/object")
+        then:
+        tmpFile.name == "object"
+
+        expect:
+        tmpFile.createNewFile()
+
+        when:
+        deleteFileElement(new Element("s3://bucket/path/to/object", null))
+        then:
+        !tmpFile.exists()
+    }
 }
