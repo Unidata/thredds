031c1f88a4552c380ca2218251f994218c63f95b
Ryan May <rmay@ucar.edu>
2015-10-28 17:35:19 -0600

    S3: Handle case when a "directory" is a valid object.
    On S3, an prefix that we consider a directory can have an object
    attached to it--even a 0 byte one. This breaks the crawling logic
    deciding whether to treat something as a collection or not.

--- cdm/src/main/java/thredds/crawlabledataset/s3/ThreddsS3ClientImpl.java
+++ cdm/src/main/java/thredds/crawlabledataset/s3/ThreddsS3ClientImpl.java
@@ -4,10 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.ListObjectsRequest;
-import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.*;
 import org.apache.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -65,6 +62,19 @@ public class ThreddsS3ClientImpl implements ThreddsS3Client {
         try {
             ObjectListing objectListing = s3Client.listObjects(listObjectsRequest);
             logger.info(String.format("Downloaded S3 listing '%s'", s3uri));
+
+            // On S3 it is possible for a prefix to be a valid object (unlike a
+            // filesystem where a node is either a file OR a directory). We
+            // exclude self here so that the "directory" isn't treated as a file
+            // by some of the caching mechanism.
+            S3ObjectSummary self = null;
+            for (S3ObjectSummary objectSummary: objectListing.getObjectSummaries()) {
+                if (objectSummary.getKey().equals(s3uri.getKeyWithTrailingDelimiter())) {
+                    self = objectSummary;
+                    break;
+                }
+            }
+            objectListing.getObjectSummaries().remove(self);
 
             if (objectListing.getObjectSummaries().isEmpty() && objectListing.getCommonPrefixes().isEmpty()) {
                 // There are no empty directories in a S3 hierarchy.
