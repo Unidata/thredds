ab97e927a1c2c6cbba3a0b9f75ac042b10f366e8
John Caron <jcaron1129@gmail.com>
2015-11-21 12:04:59 -0700

    fix unit tests

--- it/src/test/java/thredds/TestWithLocalServer.java
+++ it/src/test/java/thredds/TestWithLocalServer.java
@@ -63,8 +63,38 @@ public class TestWithLocalServer {
   }
 
   public static byte[] getContent(String endpoint, int[] expectCodes, ContentType expectContentType) {
-    return getContent(null, endpoint, expectCodes, expectContentType);
+    System.out.printf("req = '%s'%n", endpoint);
+    try (HTTPSession session = new HTTPSession(endpoint)) {
+      HTTPMethod method = HTTPFactory.Get(session);
+      int statusCode = method.execute();
+      if (statusCode != 200) {
+        System.out.printf("statusCode = %d '%s'%n", statusCode, method.getResponseAsString());
+      }
+
+      if (expectCodes == null)
+        Assert.assertEquals(200, statusCode);
+      else {
+        boolean ok = false;
+        for (int expectCode : expectCodes)
+          if (expectCode == statusCode) ok = true;
+        Assert.assertTrue(ok);
+      }
+
+      if (statusCode == 200 && expectContentType != null) {
+        Header header = method.getResponseHeader(ContentType.HEADER);
+        Assert.assertEquals(expectContentType.getContentHeader().toLowerCase(), header.getValue().toLowerCase());
+      }
+
+      return (statusCode == 200) ? method.getResponseAsBytes() : null;
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      assert false;
+    }
+
+    return null;
   }
+
 
   public static byte[] getContent(String endpoint, int expectCode, ContentType expectContentType) {
     return getContent(null, endpoint, new int[] {expectCode}, expectContentType);
