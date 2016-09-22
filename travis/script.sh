f09fa17bf51c332c4d6b78796765df71cbb944e8
cwardgar <cwardgar@ucar.edu>
2016-09-22 15:30:38 -0600

    Upload the logs generated by the 'it' module's embedded TDS to S3. * Can only occur on branches of the Unidata repo. * Improved the printing of artifact S3 URLs. * Reorganize Travis logic a bit and remove cruft. * Fix TestAdminDebug. Until now, I don't think it was doing anything remotely useful. * Added a log4j2-test.xml to "it". It can be used to change specific logger levels while debugging.

---
+++ travis/script.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [ $TASK == "docs" ]; then
+    echo Building docs
+    cd $TRAVIS_BUILD_DIR/docs/website
+    ./adoc2html.sh 2> build.log
+
+    if [ -s build.log ]; then
+      echo Doc build produced errors:
+      cat build.log
+      false
+    fi
+else
+  CONTENT_ROOT="-Dtds.content.root.path=$TRAVIS_BUILD_DIR/tds/src/test/content"
+  DOWNLOAD_DIR="-Dtds.download.dir=/tmp/download"
+  SYSTEM_PROPS="$CONTENT_ROOT $DOWNLOAD_DIR"
+
+  $TRAVIS_BUILD_DIR/gradlew $SYSTEM_PROPS --info --stacktrace testAll
+fi
