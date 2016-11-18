75ce5c281a3ecf612c84a3222584667549c6b1b7
Sean Arms <sarms@ucar.edu>
2016-11-17 17:39:04 -0700

    First pass at GeoCSV NCSS output for GridAsPoint

--- tds/src/main/java/thredds/server/ncss/format/SupportedOperation.java
+++ tds/src/main/java/thredds/server/ncss/format/SupportedOperation.java
@@ -1,34 +1,5 @@
 /*
- * Copyright 1998-2014 University Corporation for Atmospheric Research/Unidata
- *
- *   Portions of this software were developed by the Unidata Program at the
- *   University Corporation for Atmospheric Research.
- *
- *   Access and use of this software shall impose the following obligations
- *   and understandings on the user. The user is granted the right, without
- *   any fee or cost, to use, copy, modify, alter, enhance and distribute
- *   this software, and any derivative works thereof, and its supporting
- *   documentation for any purpose whatsoever, provided that this entire
- *   notice appears in all copies of the software, derivative works and
- *   supporting documentation.  Further, UCAR requests that the user credit
- *   UCAR/Unidata in any publications that result from the use of this
- *   software or in any product that includes this software. The names UCAR
- *   and/or Unidata, however, may not be used in any advertising or publicity
- *   to endorse or promote any products or commercial entity unless specific
- *   written permission is obtained from UCAR/Unidata. The user also
- *   understands that UCAR/Unidata is not obligated to provide the user with
- *   any support, consulting, training or assistance of any kind with regard
- *   to the use, operation and performance of this software nor to provide
- *   the user with any updates, revisions, new versions or "bug fixes."
- *
- *   THIS SOFTWARE IS PROVIDED BY UCAR/UNIDATA "AS IS" AND ANY EXPRESS OR
- *   IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- *   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *   DISCLAIMED. IN NO EVENT SHALL UCAR/UNIDATA BE LIABLE FOR ANY SPECIAL,
- *   INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
- *   FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
- *   NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
- *   WITH THE ACCESS, USE OR PERFORMANCE OF THIS SOFTWARE.
+ * (c) 1998-2016 University Corporation for Atmospheric Research/Unidata
  */
 
 package thredds.server.ncss.format;
@@ -52,9 +23,9 @@ public enum SupportedOperation {
     DATASET_INFO_REQUEST("Dataset info request", XML_FILE),
     DATASET_BOUNDARIES_REQUEST("Dataset grid boundaries request", WKT, JSON),
     GRID_REQUEST("Grid data request", NETCDF3, NETCDF4 /*, NETCDF4EXT*/),
-    POINT_REQUEST("Point data request", XML_STREAM, XML_FILE, CSV_STREAM, CSV_FILE, NETCDF3, NETCDF4 /*, NETCDF4EXT */),
+    POINT_REQUEST("Point data request", XML_STREAM, XML_FILE, CSV_STREAM, CSV_FILE, GEOCSV_FILE, GEOCSV_STREAM, NETCDF3, NETCDF4 /*, NETCDF4EXT */),
     STATION_REQUEST("Station data request",
-            XML_STREAM, XML_FILE, CSV_STREAM, CSV_FILE, NETCDF3, NETCDF4, /*NETCDF4EXT,*/ WATERML2);
+            XML_STREAM, XML_FILE, CSV_STREAM, CSV_FILE, GEOCSV_FILE, GEOCSV_STREAM, NETCDF3, NETCDF4, /*NETCDF4EXT,*/ WATERML2);
 
     private final String operationName;
     private final List<SupportedFormat> supportedFormats;
