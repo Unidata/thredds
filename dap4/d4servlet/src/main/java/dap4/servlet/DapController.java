788d76f47073d57820f5cb28e7486decbaaec675
Dennis Heimbigner <dmh@ucar.edu>
2017-03-24 22:01:06 -0600

    Add debugging statements

--- dap4/d4servlet/src/main/java/dap4/servlet/DapController.java
+++ dap4/d4servlet/src/main/java/dap4/servlet/DapController.java
@@ -214,6 +214,7 @@ abstract public class DapController extends HttpServlet
         }
 
         String datasetpath = DapUtil.nullify(DapUtil.canonicalpath(daprequest.getDataset()));
+        System.err.printf("xxx: context=%s%n",dapcxt.toString());
         try {
             if(datasetpath == null) {
                 // This is the case where a request was made without a dataset;
