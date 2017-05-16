b7a86918f8b271c7d5f7ee1781f0c6c42ac2ace6
James McClain <jmcclain@azavea.com>
2017-05-16 11:41:52 -0400

    S3 Unit Tests

---
+++ cdm/src/test/java/ucar/nc2/TestS3InternalCompressionRead.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 1998-2009 University Corporation for Atmospheric Research/Unidata
+ *
+ * Portions of this software were developed by the Unidata Program at the
+ * University Corporation for Atmospheric Research.
+ *
+ * Access and use of this software shall impose the following obligations
+ * and understandings on the user. The user is granted the right, without
+ * any fee or cost, to use, copy, modify, alter, enhance and distribute
+ * this software, and any derivative works thereof, and its supporting
+ * documentation for any purpose whatsoever, provided that this entire
+ * notice appears in all copies of the software, derivative works and
+ * supporting documentation.  Further, UCAR requests that the user credit
+ * UCAR/Unidata in any publications that result from the use of this
+ * software or in any product that includes this software. The names UCAR
+ * and/or Unidata, however, may not be used in any advertising or publicity
+ * to endorse or promote any products or commercial entity unless specific
+ * written permission is obtained from UCAR/Unidata. The user also
+ * understands that UCAR/Unidata is not obligated to provide the user with
+ * any support, consulting, training or assistance of any kind with regard
+ * to the use, operation and performance of this software nor to provide
+ * the user with any updates, revisions, new versions or "bug fixes."
+ *
+ * THIS SOFTWARE IS PROVIDED BY UCAR/UNIDATA "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UCAR/UNIDATA BE LIABLE FOR ANY SPECIAL,
+ * INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
+ * FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+ * WITH THE ACCESS, USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+package ucar.nc2;
+
+import junit.framework.*;
+import ucar.ma2.*;
+// import ucar.unidata.util.test.TestDir;
+
+import java.io.*;
+
+/** Test reading from S3 variable data */
+
+public class TestS3InternalCompressionRead extends TestCase {
+
+  public TestS3InternalCompressionRead( String name) {
+    super(name);
+  }
+
+  public void testNC3Read() throws IOException {
+    NetcdfFile ncfile = NetcdfFile.open("s3://noaa-nexrad-level2/2017/01/01/KTLX/KTLX20170101_030027_V06");
+
+    assert(null != ncfile.findDimension("scanR_HI"));
+    assert(null != ncfile.findDimension("gateR_HI"));
+    assert(null != ncfile.findDimension("radialR_HI"));
+
+    Variable temp = null;
+    assert(null != (temp = ncfile.findVariable("Reflectivity_HI")));
+
+    // read array
+    Array A;
+    try {
+      A = temp.read();
+    } catch (IOException e) {
+      System.err.println("ERROR reading file");
+      assert(false);
+      return;
+    }
+    assert (A.getRank() == 3);
+
+    int i,j;
+    Index ima = A.getIndex();
+    int[] shape = A.getShape();
+    assert shape[0] == 4;
+    assert shape[1] == 720;
+    assert shape[2] == 1832;
+
+    ncfile.close();
+  }
+}
