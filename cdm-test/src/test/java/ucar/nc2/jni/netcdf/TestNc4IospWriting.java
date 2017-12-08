e91c692cd93698c3bef465b8d935dde4e6b0f16b
cwardgar <cwardgar@gmail.com>
2017-12-07 22:07:09 -0700

    Test on Travis's Ubuntu 14.04.5 env using new libnetcdf 4.5.0 binaries * Other dependencies include hdf5 1.10.1 and zlib 1.2.11. * Nuked the old libnetcdf 4.4.1 binaries, which targeted Ubuntu 12.04.5.
    Remove "oraclejdk7" and "openjdk7" from Travis build matrix.
    * On Ubuntu 14, we cannot build thredds with either of those JDKs. The reason is different for each one, but neither is Gradle's fault. See https://github.com/DiUS/java-faker/pull/253
    
    @Ignore TestNc4IospWriting.expandUnlimitedDimensions()
    * The test is broken when we link against libnetcdf 4.5.0.
    * Waiting on Unidata/netcdf-c#718 to be fixed.

--- cdm-test/src/test/java/ucar/nc2/jni/netcdf/TestNc4IospWriting.java
+++ cdm-test/src/test/java/ucar/nc2/jni/netcdf/TestNc4IospWriting.java
@@ -166,7 +166,6 @@ public class TestNc4IospWriting {
 
     /////////////////////////////////////////////////
 
-    // Demonstrates GitHub issue #191. Unignore when we have a fix in place.
     @Test
     public void writeEnumType() throws IOException {
         // NetcdfFile's 0-arg constructor is protected, so must use NetcdfFileSubclass
@@ -273,6 +272,7 @@ public class TestNc4IospWriting {
     }
 
     @Test
+    @Ignore("Broken with libnetcdf 4.5.0; waiting on https://github.com/Unidata/netcdf-c/issues/718")
     public void expandUnlimitedDimensions() throws IOException, InvalidRangeException {
         File outFile = tempFolder.newFile("expandUnlimitedDimensions.nc4");
 
