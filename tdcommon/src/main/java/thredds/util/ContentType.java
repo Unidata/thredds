de4ad2c804586f7cdd04cf1961633ac503c415cb
Sean Arms <sarms@ucar.edu>
2018-02-06 16:59:09 -0700

    Add new content types for xml (wms) and png

--- tdcommon/src/main/java/thredds/util/ContentType.java
+++ tdcommon/src/main/java/thredds/util/ContentType.java
@@ -1,34 +1,6 @@
 /*
- * Copyright 1998-2014 University Corporation for Atmospheric Research/Unidata
- *
- *   Portions of this software were developed by the Unidata Program at the
- *   University Corporation for Atmospheric Research.
- *
- *   Access and use of this software shall impose the following obligations
- *   and understandings on the user. The user is granted the right, without
- *   any fee or cost, to use, copy, modify, alter, enhance and distribute
- *   this software, and any derivative works thereof, and its supporting
- *   documentation for any purpose whatsoever, provided that this entire
- *   notice appears in all copies of the software, derivative works and
- *   supporting documentation.  Further, UCAR requests that the user credit
- *   UCAR/Unidata in any publications that result from the use of this
- *   software or in any product that includes this software. The names UCAR
- *   and/or Unidata, however, may not be used in any advertising or publicity
- *   to endorse or promote any products or commercial entity unless specific
- *   written permission is obtained from UCAR/Unidata. The user also
- *   understands that UCAR/Unidata is not obligated to provide the user with
- *   any support, consulting, training or assistance of any kind with regard
- *   to the use, operation and performance of this software nor to provide
- *   the user with any updates, revisions, new versions or "bug fixes."
- *
- *   THIS SOFTWARE IS PROVIDED BY UCAR/UNIDATA "AS IS" AND ANY EXPRESS OR
- *   IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- *   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *   DISCLAIMED. IN NO EVENT SHALL UCAR/UNIDATA BE LIABLE FOR ANY SPECIAL,
- *   INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
- *   FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
- *   NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
- *   WITH THE ACCESS, USE OR PERFORMANCE OF THIS SOFTWARE.
+ * Copyright (c) 1998-2018 University Corporation for Atmospheric Research/Unidata
+ * See LICENSE.txt for license information.
  */
 
 package thredds.util;
@@ -48,9 +20,10 @@ public enum ContentType {
   ncstream("application/octet-stream", null),
   netcdf("application/x-netcdf", null),
   ogc_exception("application/vnd.ogc.se_xml", "UTF-8"),
+  png("image/png", null),
   text("text/plain", "UTF-8"),
-  xml("application/xml", "UTF-8");
-  // xml_app("text/xml; charset=UTF-8");
+  xml("application/xml", "UTF-8"),
+  xmlwms("text/xml", "iso-8859-1");
 
   public final static String HEADER = "Content-Type";
 
