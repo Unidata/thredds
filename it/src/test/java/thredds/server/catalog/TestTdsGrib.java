d79c7d4ff4c520c3a3527647c7a785322bc3e32d
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2018-07-28 08:04:18 -0600

    remove point from grid services
    In Unidata/thredds#1120 I had added ncss/point to grid services...wrong
    "point" - my bad! This commit fixes that mistake.

--- it/src/test/java/thredds/server/catalog/TestTdsGrib.java
+++ it/src/test/java/thredds/server/catalog/TestTdsGrib.java
@@ -90,7 +90,7 @@ public class TestTdsGrib {
 
     Dataset full = cat.findDatasetByID("HRRR/analysis/TP");
     Assert.assertNotNull(full);
-    Assert.assertEquals(12, full.getAccess().size());
+    Assert.assertEquals(11, full.getAccess().size());
     Assert.assertNull(full.getAccess(ServiceType.Resolver));
     Assert.assertNull(full.getAccess(ServiceType.HTTPServer));
     Assert.assertNotNull(full.getAccess(ServiceType.CdmRemote));
@@ -169,7 +169,7 @@ public class TestTdsGrib {
   public void testDefaultGribServices() throws IOException {
     String catalog = "/catalog/grib.v5/NDFD/CONUS_5km/catalog.xml";  // no service name, should use GRID default
     Catalog cat = TdsLocalCatalog.open(catalog);
-    testCat(cat, 12, true, null, 0);
+    testCat(cat, 11, true, null, 0);
 
     Dataset top = cat.getDatasetsLocal().get(0);
     Assert.assertTrue(!top.hasAccess());
@@ -180,7 +180,7 @@ public class TestTdsGrib {
       } else {
         CatalogRef catref = (CatalogRef) ds;
         Catalog cat2 = TdsLocalCatalog.openFromURI(catref.getURI());
-        testCat(cat2, 12, false, "GridServices", 13);
+        testCat(cat2, 11, false, "GridServices", 12);
         break;
       }
     }
