c33d622c843f8fa051c3c2c790b2d1898cf04250
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2019-02-28 15:55:50 -0700

    update IFPSConvention and add test
    addresses Unidata/thredds#1240

--- cdm-test/src/test/java/ucar/nc2/dataset/TestConventions.java
+++ cdm-test/src/test/java/ucar/nc2/dataset/TestConventions.java
@@ -10,18 +10,26 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+
 import ucar.ma2.DataType;
 import ucar.nc2.dt.GridCoordSystem;
 import ucar.nc2.dt.grid.GeoGrid;
 import ucar.nc2.dt.grid.GridDataset;
+import ucar.nc2.ft2.coverage.Coverage;
+import ucar.nc2.ft2.coverage.CoverageCollection;
+import ucar.nc2.ft2.coverage.CoverageCoordSys;
+import ucar.nc2.ft2.coverage.CoverageDatasetFactory;
+import ucar.nc2.ft2.coverage.FeatureDatasetCoverage;
+import ucar.nc2.ft2.coverage.HorizCoordSys;
 import ucar.nc2.time.Calendar;
 import ucar.nc2.time.CalendarDate;
 import ucar.nc2.time.CalendarDateFormatter;
-import ucar.unidata.util.test.category.NeedsCdmUnitTest;
+import ucar.nc2.util.Optional;
 import ucar.unidata.util.test.TestDir;
-
-import java.io.IOException;
-import java.lang.invoke.MethodHandles;
+import ucar.unidata.util.test.category.NeedsCdmUnitTest;
 
 /**
  * Test specific files for CoordSys Conventions
@@ -71,6 +79,21 @@ public class TestConventions  {
       assert gcs.isLatLon();
       Assert.assertEquals(DataType.BYTE, grid.getDataType());
     }
+  }
+
+  @Test
+  public void testIfps() throws IOException {
+    Optional<FeatureDatasetCoverage> ds =
+            CoverageDatasetFactory.openCoverageDataset(TestDir.cdmUnitTestDir + "conventions/ifps/HUNGrids.netcdf");
+    assert ds.isPresent();
+    CoverageCollection cc = ds.get().getSingleCoverageCollection();
+    assert cc != null;
+    Coverage coverage = cc.findCoverage("T_SFC");
+    assert coverage != null;
+    CoverageCoordSys cs = coverage.getCoordSys();
+    HorizCoordSys hcs = cs.getHorizCoordSys();
+    assert hcs.isProjection();
+    Assert.assertEquals(DataType.FLOAT, coverage.getDataType());
   }
 
 }
