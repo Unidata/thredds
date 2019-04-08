ae9cb5567d08eb7e83fe040a1c53d8a0f007b660
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2019-04-08 10:19:15 -0600

    use v1 API for nexus raw repository publishing

--- buildSrc/src/main/groovy/edu/ucar/build/publishing/PublishToRawRepoTask.groovy
+++ buildSrc/src/main/groovy/edu/ucar/build/publishing/PublishToRawRepoTask.groovy
@@ -99,7 +99,7 @@ class PublishToRawRepoTask extends DefaultTask {
     
         HttpBuilder http = OkHttpBuilder.configure {
             request.uri = host
-            request.uri.path = "/service/rest/beta/components"
+            request.uri.path = "/service/rest/v1/components"
             request.uri.query['repository'] = repoName
             
             request.contentType = ContentTypes.MULTIPART_FORMDATA.first()
