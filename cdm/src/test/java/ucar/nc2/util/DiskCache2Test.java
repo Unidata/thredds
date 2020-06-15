854dda970723fc7ff6a9554a9eb51edb430408d6
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2020-06-15 11:52:38 -0600

    Fix DiskCache2 createUniqueFile method

--- cdm/src/test/java/ucar/nc2/util/DiskCache2Test.java
+++ cdm/src/test/java/ucar/nc2/util/DiskCache2Test.java
@@ -1,5 +1,7 @@
 package ucar.nc2.util;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.*;
 
 import java.io.File;
@@ -14,15 +16,16 @@ import java.nio.file.Paths;
  */
 public class DiskCache2Test {
 
-  private static Path testCacheDir;
   private static Path dirForStaticTests;
   private static DiskCache2 diskCache;
+
+  private static List<File> filesToDelete = new ArrayList<>();
 
   @BeforeClass
   public static void beforeClass() throws IOException {
     String cacheName = DiskCache2Test.class.getSimpleName();
     dirForStaticTests = Files.createTempDirectory(cacheName + "-static-tests");
-    testCacheDir = Files.createTempDirectory(cacheName);
+    Path testCacheDir = Files.createTempDirectory(cacheName);
 
     try {
       Files.deleteIfExists(testCacheDir);
@@ -50,26 +53,25 @@ public class DiskCache2Test {
   @Test
   public void canWriteFile() throws IOException {
     File file = Files.createTempFile(dirForStaticTests, "temp", null).toFile();
-
-    try {
-      Assert.assertTrue(file.exists());
-      Assert.assertTrue(DiskCache2.canWrite(file));
-    } finally {
-      file.delete();
-    }
+    Assert.assertTrue(file.exists());
+    filesToDelete.add(file);
+    Assert.assertTrue(DiskCache2.canWrite(file));
   }
 
   @Test
   public void cantWriteFile() throws IOException {
     File file = Files.createTempFile(dirForStaticTests, "temp", null).toFile();
+    filesToDelete.add(file);
 
-    try {
-      Assert.assertTrue(file.exists());
-      Assert.assertTrue(file.setWritable(false));
-      Assert.assertFalse(DiskCache2.canWrite(file));
-    } finally {
-      file.delete();
-    }
+    Assert.assertTrue(file.exists());
+
+    // make unwritable
+    Assert.assertTrue(file.setWritable(false));
+    Assert.assertFalse(DiskCache2.canWrite(file));
+
+    // change back to writable so we can clean up
+    Assert.assertTrue(file.setWritable(true));
+    Assert.assertTrue(DiskCache2.canWrite(file));
   }
 
   @Test
@@ -93,36 +95,58 @@ public class DiskCache2Test {
   public void checkUniqueFileNames() throws IOException {
     final String prefix  = "pre-";
     final String suffix  = "-suf";
+    final String lockFileExtention = ".reserve";
 
-    File first = diskCache.createUniqueFile(prefix, suffix);
-    File second = diskCache.createUniqueFile(prefix, suffix);
+    // loop a few times to make sure it triggers.
+    for (int i = 0; i < 5; i++){
+      File first = diskCache.createUniqueFile(prefix, suffix);
+      File second = diskCache.createUniqueFile(prefix, suffix);
 
-    // files do not exist yet
-    Assert.assertFalse(first.exists());
-    Assert.assertFalse(second.exists());
+      // files do not exist yet
+      Assert.assertFalse(first.exists());
+      Assert.assertFalse(second.exists());
 
-    // make the files
-    first.createNewFile();
-    second.createNewFile();
+      // make the files
+      first.createNewFile();
+      second.createNewFile();
 
-    // files should exist now
-    Assert.assertTrue(first.exists());
-    Assert.assertTrue(second.exists());
+      // keep track of files that need to be cleaned up
+      filesToDelete.add(first);
+      filesToDelete.add(second);
 
-    // make sure they start with prefix
-    Assert.assertTrue(first.getName().startsWith(prefix));
-    Assert.assertTrue(second.getName().startsWith(prefix));
+      // files should exist now
+      Assert.assertTrue(first.exists());
+      Assert.assertTrue(second.exists());
 
-    // make sure they end with suffix
-    Assert.assertTrue(first.getName().endsWith(suffix));
-    Assert.assertTrue(second.getName().endsWith(suffix));
+      // lock files should exist as well
+      File firstLockFile = Paths.get(first.getCanonicalFile().toString() + lockFileExtention).toFile();
+      File secondLockFile = Paths.get(second.getCanonicalFile().toString() + lockFileExtention).toFile();
+      Assert.assertTrue(firstLockFile.exists());
+      Assert.assertTrue(secondLockFile.exists());
+      filesToDelete.add(firstLockFile);
+      filesToDelete.add(secondLockFile);
 
-    // make sure they are different files
-    Assert.assertNotEquals(first.getAbsolutePath(), second.getAbsolutePath());
+      // make sure they start with prefix
+      Assert.assertTrue(first.getName().startsWith(prefix));
+      Assert.assertTrue(second.getName().startsWith(prefix));
+
+      // make sure they end with suffix
+      Assert.assertTrue(first.getName().endsWith(suffix));
+      Assert.assertTrue(second.getName().endsWith(suffix));
+
+      // make sure they are different files
+      Assert.assertNotEquals(first.getAbsolutePath(), second.getAbsolutePath());
+    }
   }
 
   @AfterClass
   public static void afterClass() throws IOException {
+    for (File f : filesToDelete) {
+      Files.deleteIfExists(f.toPath());
+    }
+    Files.delete(dirForStaticTests);
+
     diskCache.exit();
+    Files.deleteIfExists(Paths.get(diskCache.getRootDirectory()));
   }
 }
